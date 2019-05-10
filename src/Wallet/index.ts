df647322cd0b64c32917cf98819cb7f24f178c5d
Sam Jeston <sam.jeston@iohk.io>
2019-05-10 14:40:48 +1000

    Import cleanup

--- src/Wallet/index.ts
+++ src/Wallet/index.ts
@@ -3,5 +3,5 @@ import { Wallet } from './Wallet'
 export { Address, AddressType } from './Address'
 export { Utxo, UtxoWithAddressing } from './Utxo'
 export { InputSelectionAlgorithm } from './InputSelectionAlgorithm'
-export { addressDiscoveryWithinBounds } from './lib/address_discovery'
+export { addressDiscoveryWithinBounds } from './lib'
 export default Wallet
