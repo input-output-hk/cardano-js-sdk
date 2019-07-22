5ad7ffd86f7def2a6c64537d41bb9630f95d9de3
Sam Jeston <Sam-Jeston@users.noreply.github.com>
2019-07-23 09:27:32 +1000

    Chore/ci and license (#37)
    Also remove the lodash vulnerability

--- src/test/RemoteWalletIntegration.spec.ts
+++ src/test/RemoteWalletIntegration.spec.ts
@@ -46,7 +46,7 @@ describe('Example: Interacting with remote wallets', function () {
     describe('list wallets', () => {
       it('allows the connection to list existing wallets', async () => {
         const wallets = await connection.listWallets()
-        expect(wallets.length >= 10).to.eql(true)
+        expect(wallets.length >= 1).to.eql(true)
       })
     })
 
