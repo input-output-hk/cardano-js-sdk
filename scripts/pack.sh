1afc51e8f15c9ba5b35f4d284dc0a182eede0292
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-09-24 18:39:54 +1000

    chore: add new packages to pack and publish scripts

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,5 +2,12 @@
 
 set -euo pipefail
 
+npm pack --cwd ./packages/blockfrost && \
+npm pack --cwd ./packages/cardano-graphql-db-sync && \
+npm pack --cwd ./packages/cardano-serialization-lib && \
+npm pack --cwd ./packages/cip2 && \
+npm pack --cwd ./packages/cip30 && \
 npm pack --cwd ./packages/core && \
-npm pack --cwd ./packages/golden-test-generator
+npm pack --cwd ./packages/golden-test-generator && \
+npm pack --cwd ./packages/in-memory-key-manager && \
+npm pack --cwd ./packages/wallet
