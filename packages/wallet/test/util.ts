745c383b390314d575d4373df567c13fb999d128
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-09-24 21:52:46 +1000

    chore(wallet): add non-critical todo

--- packages/wallet/test/util.ts
+++ packages/wallet/test/util.ts
@@ -1,5 +1,6 @@
 import { SelectionConstraints } from '@cardano-sdk/cip2';
 
+// Todo: Hoist to util-dev package
 export const NO_CONSTRAINTS: SelectionConstraints = {
   computeMinimumCoinQuantity: () => 1_000_000n,
   computeMinimumCost: async () => 170_000n,
