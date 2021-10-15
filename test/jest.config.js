2758594dfb1d77e805f2aa6df427c3bd0ad7c617
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2021-10-15 13:23:50 +0300

    fix: change build target modules to esm (BREAKING)

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  setupFilesAfterEnv: [require.resolve('./jest.setup.js')],
-  preset: 'ts-jest',
-  transform: {
-    "^.+\\.test.ts?$": "ts-jest"
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    // Alternatively could use this, but it takes a very long time to build without cache (~10 times longer)
+    // babel instead of tsc to transform js files would probably be faster
+    // transformIgnorePatterns: ["/node_modules/(?!(lodash-es)/)"],
+    "lodash-es": "lodash"
   },
   coveragePathIgnorePatterns: ['\.config\.js'],
   testTimeout: process.env.CI ? 120000 : 12000,
--- test/jest.setup.js
+++
@@ -1,4 +0,0 @@
-// TODO: jest environment is not happy with 'lodash-es' exports.
-// I think using non-es-module 'lodash' in 'dependencies' is too heavy.
-// eslint-disable-next-line unicorn/prefer-module
-jest.mock('lodash-es', () => require('lodash'));
