a831aefd732a9ba9b1a6657d9bbe9ede8e9b95b9
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2021-11-12 13:47:19 +0200

    test(wallet): fix e2e test for SingleAddressWallet.balance

--- packages/wallet/test/e2e/SingleAddressWallet.test.ts
+++ packages/wallet/test/e2e/SingleAddressWallet.test.ts
@@ -1,9 +1,10 @@
+import { Cardano } from '@cardano-sdk/core';
 import { SingleAddressWallet, Wallet } from '../../src';
 import { filter, firstValueFrom, tap } from 'rxjs';
 import { keyManager, stakePoolSearchProvider, walletProvider } from './config';
 
 const faucetAddress =
-  'addr_test1qrydm8hsalwjmuqj624cwnyrs554zu6a8n8wg64dxk3zarsxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknsq3qxgd';
+  'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3';
 
 describe('SingleAddressWallet', () => {
   let wallet: Wallet;
@@ -33,18 +34,27 @@ describe('SingleAddressWallet', () => {
     expect(initialTotalBalance.coins).toBe(initialAvailableBalance.coins);
     // available balance changes when tx is submitted
     const txCoins = 1_000_000n;
-    const expectedCoinsAfterTx = initialTotalBalance.coins - txCoins;
     const txInternals = await wallet.initializeTx({
       outputs: new Set([{ address: faucetAddress, value: { coins: txCoins } }])
     });
     await wallet.submitTx(await wallet.finalizeTx(txInternals));
+
     const afterTxTotalBalance = await firstValueFrom(wallet.balance.total$);
     const afterTxAvailableBalance = await firstValueFrom(wallet.balance.available$);
     expect(afterTxTotalBalance.coins).toBe(initialTotalBalance.coins);
-    expect(afterTxAvailableBalance.coins).toBe(expectedCoinsAfterTx);
+
+    const utxo = wallet.utxo.total$.value!;
+    const expectedCoinsWhileTxPending =
+      initialTotalBalance.coins -
+      Cardano.util.coalesceValueQuantities(
+        txInternals.body.inputs.map((txInput) => utxo.find(([txIn]) => txIn.txId === txInput.txId)![1].value)
+      ).coins;
+    expect(afterTxAvailableBalance.coins).toBe(expectedCoinsWhileTxPending);
+
+    const expectedAfterTxCoins = initialTotalBalance.coins - txCoins - txInternals.body.fee;
     await firstValueFrom(
       wallet.balance.total$.pipe(
-        filter(({ coins }) => coins === expectedCoinsAfterTx),
+        filter(({ coins }) => coins === expectedAfterTxCoins),
         tap(({ coins }) => expect(wallet.balance.available$.value?.coins).toBe(coins))
       )
     );
