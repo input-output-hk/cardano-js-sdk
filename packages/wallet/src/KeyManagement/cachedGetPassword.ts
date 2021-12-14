fa67b9e6ddcba788c3e13f850f7d31f6b1bba7d9
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2021-12-14 09:07:49 +0200

    fix(wallet): cache cachedGetPassword calls to getPassword instead of result

--- packages/wallet/src/KeyManagement/cachedGetPassword.ts
+++ packages/wallet/src/KeyManagement/cachedGetPassword.ts
@@ -2,13 +2,20 @@ import { GetPassword } from './types';
 import { Milliseconds } from '..';
 
 export const cachedGetPassword = (getPassword: () => Promise<Uint8Array>, cacheDuration: Milliseconds): GetPassword => {
-  let cached: Uint8Array | null;
+  let cached: Promise<Uint8Array> | null;
   let timeout: NodeJS.Timeout | null;
-  return async (noCache) => {
+  return (noCache) => {
     if (noCache || !cached) {
-      cached = await getPassword();
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(() => (cached = null), cacheDuration);
+      cached = getPassword()
+        .then((password) => {
+          if (timeout) clearTimeout(timeout);
+          timeout = setTimeout(() => (cached = null), cacheDuration);
+          return password;
+        })
+        .catch((error) => {
+          cached = null;
+          throw error;
+        });
     }
     return cached;
   };
