0f4cb2835efc9baf8a0e2d7921da59fe70fae1d7
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-01-11 18:35:23 +0200

    feat(core): partial support for legacy base58 addresses

--- packages/core/src/Cardano/types/Address.ts
+++ packages/core/src/Cardano/types/Address.ts
@@ -1,13 +1,20 @@
-import * as util from '../util';
+import * as typesUtil from '../util';
+import { InvalidStringError } from '../..';
+import { util as addressUtil } from '../../Address';
 
 /**
  * mainnet or testnet address as bech32 string, consisting of
  * network tag, payment credential and optional stake credential
  */
-export type Address = util.OpaqueString<'Address'>;
+export type Address = typesUtil.OpaqueString<'Address'>;
 
 /**
- * @param {string} value mainnet or testnet address as bech32 string
+ * @param {string} value mainnet or testnet address
  * @throws InvalidStringError
  */
-export const Address = (value: string): Address => util.typedBech32(value, ['addr', 'addr_test'], [47, 92]);
+export const Address = (value: string): Address => {
+  if (addressUtil.isAddress(value)) {
+    return value as unknown as Address;
+  }
+  throw new InvalidStringError(`Invalid address: ${value}`);
+};
