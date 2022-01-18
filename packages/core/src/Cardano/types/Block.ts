d1c098cc4dcd34421336cc0516d0a2a59ff355e1
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-01-18 15:51:10 +0200

    fix(core)!: add support to genesis delegate as slot leader

--- packages/core/src/Cardano/types/Block.ts
+++ packages/core/src/Cardano/types/Block.ts
@@ -1,7 +1,7 @@
 import { BlockNo, BlockSize, Slot } from '@cardano-ogmios/schema';
-import { Cardano } from '../..';
 import { Epoch, Lovelace, PoolId } from '.';
-import { Hash32ByteBase16, OpaqueString, typedBech32 } from '../util';
+import { Hash28ByteBase16, Hash32ByteBase16, OpaqueString, typedBech32 } from '../util';
+import { InvalidStringError } from '../..';
 
 export { BlockNo } from '@cardano-ogmios/schema';
 
@@ -32,15 +32,41 @@ export { BlockSize };
 export type VrfVkBech32 = OpaqueString<'VrfVkBech32'>;
 export const VrfVkBech32 = (value: string) => typedBech32<VrfVkBech32>(value, 'vrf_vk', 52);
 
+/**
+ * Shelley genesis delegate
+ * Either a 28 byte hex string, or 'ShelleyGenesis-[8byte-hex-string]'
+ */
+export type GenesisDelegate = OpaqueString<'GenesisDelegate'>;
+export const GenesisDelegate = (value: string): GenesisDelegate => {
+  // eslint-disable-next-line wrap-regex
+  if (/ShelleyGenesis-[\da-f]{16}/.test(value)) {
+    return value as unknown as GenesisDelegate;
+  }
+  return Hash28ByteBase16(value);
+};
+
+export type SlotLeader = PoolId | GenesisDelegate;
+export const SlotLeader = (value: string): SlotLeader => {
+  try {
+    return PoolId(value);
+  } catch {
+    try {
+      return GenesisDelegate(value);
+    } catch (error) {
+      throw new InvalidStringError('Expected either PoolId or GenesisDelegate', error);
+    }
+  }
+};
+
 export interface Block {
   header: PartialBlockHeader;
   date: Date;
   epoch: Epoch;
   epochSlot: number;
-  slotLeader: PoolId;
+  slotLeader: SlotLeader;
   size: BlockSize;
   txCount: number;
-  totalOutput: Cardano.Lovelace;
+  totalOutput: Lovelace;
   fees: Lovelace;
   vrf: VrfVkBech32;
   previousBlock?: BlockId;
