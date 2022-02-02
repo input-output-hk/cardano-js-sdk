6ae18a6915d771baef6d7104dfaf0f1054f93be8
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-02-02 17:22:06 +0200

    feat(core): add optional 'options' argument to StakePoolSearchProvider.queryStakePools

--- packages/core/src/Provider/StakePoolSearchProvider/types/StakePoolSearchProvider.ts
+++ packages/core/src/Provider/StakePoolSearchProvider/types/StakePoolSearchProvider.ts
@@ -1,10 +1,25 @@
 import { Cardano } from '../../..';
 
+export interface StakePoolQueryOptions {
+  /**
+   * Will fetch all stake pool reward history if not specified
+   */
+  rewardsHistoryLimit?: number;
+  /**
+   * Will return all stake pools matching the query if not specified
+   */
+  pagination?: {
+    startAt: number;
+    limit: number;
+  };
+}
+
 export interface StakePoolSearchProvider {
   /**
    * @param {string[]} query an array of partial pool data: bech32 ID, name, ticker
-   * @returns Stake pools that match any fragment.
+   * @param {StakePoolQueryOptions} options query options
+   * @returns Stake pools that match any fragment
    * @throws ProviderError
    */
-  queryStakePools: (fragments: string[]) => Promise<Cardano.StakePool[]>;
+  queryStakePools: (fragments: string[], options?: StakePoolQueryOptions) => Promise<Cardano.StakePool[]>;
 }
