0ce3088d99699c60d255877403d3544356beffdd
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-02-02 17:08:24 +0200

    feat(core): add StakePool.epochRewards and estimateStakePoolAPY util

--- packages/core/src/Cardano/util/index.ts
+++ packages/core/src/Cardano/util/index.ts
@@ -1,5 +1,6 @@
 export * from './coalesceValueQuantities';
 export * from './computeMinUtxoValue';
 export * from './computeImplicitCoin';
+export * from './estimateStakePoolAPY';
 export * from './primitives';
 export * as metadatum from './metadatum';
