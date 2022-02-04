de488b1b63ca0c43b2a302b82e7111c224f25d43
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-02-04 17:08:09 +0200

    chore: add a script to patch jest-worker for bigint assertion messages

---
+++ scripts/patch-jest-worker.js
@@ -0,0 +1,26 @@
+// See https://github.com/facebook/jest/issues/11617
+const fs = require('fs');
+const path = require('path');
+
+const pathToMessageParentJs = path.join(__dirname, '../node_modules/jest-worker/build/workers/messageParent.js');
+
+const originalContents = fs.readFileSync(pathToMessageParentJs).toString();
+const patch = `try {
+  parentProcess.send([_types().PARENT_MESSAGE_CUSTOM, message]);
+} catch (error) {
+  console.error('jest-worker message serialisation failed', error);
+  console.dir(message, {depth: 10});
+  throw error;
+}`;
+
+if (originalContents.includes(patch)) {
+  console.log(pathToMessageParentJs, 'is already patched, nothing to do...');
+} else {
+  const patchedContents = originalContents.replace(
+    'parentProcess.send([_types().PARENT_MESSAGE_CUSTOM, message]);',
+    patch
+  );
+
+  fs.writeFileSync(pathToMessageParentJs, patchedContents);
+  console.log(pathToMessageParentJs, 'successfully patched!');
+}
