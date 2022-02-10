834f93d4699890d89c9cc27656f0bf60c2033163
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-02-10 19:19:16 +0200

    feat(cardano-graphql): add object types for Asset and its metadatas

--- packages/cardano-graphql/src/Schema/types/PublicKey.ts
+++ packages/cardano-graphql/src/Schema/types/PublicKey.ts
@@ -1,6 +1,7 @@
 import { Address, RewardAccount } from './Address';
 import { Cardano } from '@cardano-sdk/core';
 import { Directive, Field, ObjectType } from 'type-graphql';
+import { Policy } from './Asset/Policy';
 import { Signature } from './Transaction/Signature';
 import { Transaction } from './Transaction';
 
@@ -9,17 +10,21 @@ export class PublicKey {
   @Directive('@id')
   @Field(() => String, { description: 'hex-encoded Ed25519 public key hash' })
   hash: Cardano.Ed25519KeyHash;
-  @Field(() => String, { description: 'hex-encoded Ed25519 public key' })
-  key?: Cardano.Ed25519PublicKey;
-  @Field(() => [Transaction])
-  requiredExtraSignatureInTransactions: Transaction[];
+  // eslint-disable-next-line sonarjs/no-duplicate-string
   @Directive('@hasInverse(field: publicKey)')
   @Field(() => [Signature])
   signatures: Signature[];
+  @Field(() => String, { description: 'hex-encoded Ed25519 public key' })
+  key: Cardano.Ed25519PublicKey;
+  @Field(() => [Transaction], { nullable: true })
+  requiredExtraSignatureInTransactions?: Transaction[];
   @Directive('@hasInverse(field: paymentPublicKey)')
   @Field(() => [Address], { nullable: true })
   addresses?: Address[];
   @Directive('@hasInverse(field: publicKey)')
   @Field(() => RewardAccount, { nullable: true })
   rewardAccount?: RewardAccount;
+  @Directive('@hasInverse(field: publicKey)')
+  @Field(() => [Policy], { nullable: true })
+  policies?: Policy;
 }
