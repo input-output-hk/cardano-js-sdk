1528bbcebb5dc25a70ccd968f249a1bfad59233b
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-03-11 10:15:57 +0200

    test(blockfrost): fix e2e tests config

--- packages/blockfrost/test/e2e/config.ts
+++ packages/blockfrost/test/e2e/config.ts
@@ -1,7 +1,9 @@
+import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 import { blockfrostAssetProvider, blockfrostWalletProvider } from '../../src';
 
 const testnetprojectId = process.env.BLOCKFROST_API_KEY;
 if (!testnetprojectId) throw new Error('BLOCKFROST_API_KEY not set');
 
-export const walletProvider = blockfrostWalletProvider({ isTestnet: true, projectId: testnetprojectId });
-export const assetProvider = blockfrostAssetProvider({ isTestnet: true, projectId: testnetprojectId });
+const blockfrostApi = new BlockFrostAPI({ isTestnet: true, projectId: testnetprojectId });
+export const walletProvider = blockfrostWalletProvider(blockfrostApi);
+export const assetProvider = blockfrostAssetProvider(blockfrostApi);
