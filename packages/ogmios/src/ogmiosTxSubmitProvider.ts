3b8461b2ca9081736c1495318be68deb0e12bd6b
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2022-03-15 15:52:34 +0100

    feat(ogmios): package init and ogmiosTxSubmitProvider
    - Uses a mock Ogmios server, that we can also use to facilitate chain-sync testing.
    - random free port selected within the test

---
+++ packages/ogmios/src/ogmiosTxSubmitProvider.ts
@@ -0,0 +1,54 @@
+import { Buffer } from 'buffer';
+import { Cardano, TxSubmitProvider } from '@cardano-sdk/core';
+import {
+  ConnectionConfig,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  TxSubmission,
+  UnknownResultError,
+  createInteractionContext,
+  createTxSubmissionClient
+} from '@cardano-ogmios/client';
+import { Logger, dummyLogger } from 'ts-log';
+
+/**
+ * Connect to an [Ogmios](https://ogmios.dev/) instance
+ *
+ * @param {ConnectionConfig} connectionConfig Ogmios connection configuration
+ * @param {Logger} logger object implementing the Logger abstract class
+ * @returns {TxSubmitProvider} TxSubmitProvider
+ * @throws {TxSubmission.errors}
+ */
+export const ogmiosTxSubmitProvider = (
+  connectionConfig: ConnectionConfig,
+  logger: Logger = dummyLogger
+): TxSubmitProvider => ({
+  submitTx: async (signedTransaction) => {
+    // The Ogmios client supports opening a long-running ws connection,
+    // however as the provider interface doesn't include shutdown handling,
+    // we're using the one time interaction type for now.
+    try {
+      const interactionContext = await createInteractionContext(
+        (error) => {
+          logger.error({ error: error.name, module: 'ogmiosTxSubmitProvider' }, error.message);
+        },
+        logger.info,
+        {
+          connection: connectionConfig,
+          interactionType: 'OneTime'
+        }
+      );
+      const txSubmissionClient = await createTxSubmissionClient(interactionContext);
+      const txHex = Buffer.from(signedTransaction).toString('hex');
+      return await txSubmissionClient.submitTx(txHex);
+    } catch (error) {
+      // Ogmios throws an array of domain errors, or a single catch-all if the request is malformed.
+      // There's currently no utility to invert this logic to ensure other non-UnknownResultErrors are
+      // also re-thrown as the UnknownTxSubmissionError, but it's a minor issue we can improve on.
+      if (error instanceof UnknownResultError) {
+        throw new Cardano.TxSubmissionErrors.UnknownTxSubmissionError(error);
+      }
+      throw error;
+    }
+  }
+});
