d93a121c626e7c9ce060d575802bc2775cf875e3
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2022-03-16 17:49:16 +0100

    feat(cardano-graphql-services): module logger

---
+++ packages/util-dev/src/stubLogger.ts
@@ -0,0 +1,9 @@
+import { Logger } from 'ts-log';
+
+export const createStubLogger = (): Logger => ({
+  debug: jest.fn(),
+  error: jest.fn(),
+  info: jest.fn(),
+  trace: jest.fn(),
+  warn: jest.fn()
+});
