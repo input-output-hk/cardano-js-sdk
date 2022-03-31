67e316ed583b335a5400842a250ca04965d8e66b
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-03-31 17:32:35 +0300

    fix(cip30): make handleMessages more resilient

--- packages/cip30/src/BrowserExtension/handleMessages.ts
+++ packages/cip30/src/BrowserExtension/handleMessages.ts
@@ -1,20 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Logger, dummyLogger } from 'ts-log';
-import { Message } from './types';
 import { WalletApi } from '../Wallet';
 import browser from 'webextension-polyfill';
 
-export const handleMessages = (walletApi: WalletApi, logger: Logger = dummyLogger): void => {
-  browser.runtime.onMessage.addListener(async (msg: Message) => {
+export const handleMessages = (walletApi: WalletApi, logger: Logger = dummyLogger) => {
+  const listener = async (msg: unknown) => {
     logger.debug('new message received: ', msg);
 
-    const walletMethod = walletApi[msg.method];
-
-    if (!walletMethod) {
-      logger.error(`No method implemented for ${msg.method}`);
-
+    if (typeof msg !== 'object' || msg === null || !['method', 'arguments'].every((prop) => prop in msg)) {
+      // Probably a message that not intended to this handler
       return;
     }
 
-    return walletMethod(...msg.arguments);
-  });
+    const { method, arguments: args } = msg as any;
+    if (typeof method !== 'string' || !Array.isArray(args) || !(method in walletApi)) {
+      logger.error('Invalid Message', msg);
+      return;
+    }
+
+    return (walletApi as any)[method](...args);
+  };
+  browser.runtime.onMessage.addListener(listener);
+  return () => browser.runtime.onMessage.removeListener(listener);
 };
