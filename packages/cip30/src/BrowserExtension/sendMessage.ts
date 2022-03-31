f4bbd2d224c90dec8a535236dd013d9fc2b7df22
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-03-31 18:07:27 +0300

    feat(cip30): add extension ID prop

--- packages/cip30/src/BrowserExtension/sendMessage.ts
+++ packages/cip30/src/BrowserExtension/sendMessage.ts
@@ -3,12 +3,22 @@ import { Message } from './types';
 import { WalletApi } from '../Wallet';
 import browser from 'webextension-polyfill';
 
-export const sendMessage = async (msg: Message, logger: Logger = dummyLogger): Promise<WalletApi[keyof WalletApi]> => {
-  logger.debug('sendMessage', msg);
-  try {
-    return browser.runtime.sendMessage(msg);
-  } catch (error) {
-    logger.error('sendMessage', error);
-    throw error;
-  }
-};
+export interface MessengerProps {
+  extensionId?: string;
+  logger?: Logger;
+}
+
+export const createMessenger =
+  ({ logger = dummyLogger, extensionId }: MessengerProps = {}) =>
+  async (msg: Message): Promise<WalletApi[keyof WalletApi]> => {
+    logger.debug('sendMessage', msg);
+    try {
+      if (extensionId) {
+        return browser.runtime.sendMessage(extensionId, msg);
+      }
+      return browser.runtime.sendMessage(msg);
+    } catch (error) {
+      logger.error('sendMessage', error);
+      throw error;
+    }
+  };
--- packages/cip30/src/BrowserExtension/uiWallet.ts
+++ packages/cip30/src/BrowserExtension/uiWallet.ts
@@ -1,9 +1,14 @@
 import { Logger, dummyLogger } from 'ts-log';
 import { Message } from './types';
 import { WalletApi } from '..';
-import { sendMessage } from './sendMessage';
+import { createMessenger } from './sendMessage';
 
-export const createUiWallet = (logger: Logger = dummyLogger): WalletApi => {
+export interface CreateUiWalletProps {
+  walletExtensionId?: string;
+  logger?: Logger;
+}
+
+export const createUiWallet = ({ logger = dummyLogger, walletExtensionId }: CreateUiWalletProps = {}): WalletApi => {
   const methodNames: (keyof WalletApi)[] = [
     'getNetworkId',
     'getUtxos',
@@ -16,12 +21,13 @@ export const createUiWallet = (logger: Logger = dummyLogger): WalletApi => {
     'signData',
     'submitTx'
   ];
+  const sendMessage = createMessenger({ extensionId: walletExtensionId, logger });
   return <WalletApi>(
     (<unknown>(
       Object.fromEntries(
         methodNames.map((method) => [
           method,
-          (...args: Message['arguments']) => sendMessage({ arguments: args, method }, logger)
+          (...args: Message['arguments']) => sendMessage({ arguments: args, method })
         ])
       )
     ))
