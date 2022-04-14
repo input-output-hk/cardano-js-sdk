65003b5314900e15f5842bb9518b35a69b6931b8
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2022-04-14 15:39:09 +1000

    feat(core): isAddressWithin + isOutgoing address utils

--- packages/core/src/Address/util.ts
+++ packages/core/src/Address/util.ts
@@ -1,6 +1,22 @@
+import { Address, TxAlonzo } from '../Cardano';
 import { parseCslAddress } from '../CSL';
 
 /**
  * Validate input as a Cardano Address from all Cardano eras and networks
  */
 export const isAddress = (input: string): boolean => !!parseCslAddress(input);
+
+/**
+ * Checks that an object containing an address (e.g., output, input) is within a set of provided addresses
+ */
+export const isAddressWithin =
+  (addresses: Address[]) =>
+  ({ address }: { address: Address }): boolean =>
+    addresses.includes(address!);
+
+/**
+ * Receives a transaction and a set of addresses to check if the transaction is outgoing,
+ * i.e., some of the addresses are included in the transaction inputs
+ */
+export const isOutgoing = (tx: TxAlonzo, ownAddresses: Address[]): boolean =>
+  tx.body.inputs.some(isAddressWithin(ownAddresses));
