bd22262cdac4d90561069fefe89028eaf01643a0
Daniele Ricci <daniele.ricci@iohk.io>
2022-05-26 17:23:54 +0200

    feat(ogmios): added urlToConnectionConfig function

--- packages/ogmios/src/ogmiosTxSubmitProvider.ts
+++ packages/ogmios/src/ogmiosTxSubmitProvider.ts
@@ -59,3 +59,15 @@ export const ogmiosTxSubmitProvider = (
     }
   }
 });
+
+/**
+ * Converts an Ogmios connection URL to a Ogmios ConnectionConfig Object
+ *
+ * @param {URL} connectionURL Ogmios connection URL
+ * @returns {ConnectionConfig} the ConnectionConfig Object
+ */
+export const urlToConnectionConfig = (connectionURL?: URL): ConnectionConfig => ({
+  host: connectionURL?.hostname,
+  port: connectionURL ? Number.parseInt(connectionURL.port) : undefined,
+  tls: connectionURL?.protocol === 'wss'
+});
