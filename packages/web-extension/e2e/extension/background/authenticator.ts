3c7b9900b53e45a9f037f6c7fa80752bcea81ee0
Angel Castillo <angel.castillob@protonmail.com>
2022-06-28 10:49:20 +0800

    refactor(e2e): remove web extension e2e test from web-extension package

--- packages/web-extension/e2e/extension/background/authenticator.ts
+++
@@ -1,8 +0,0 @@
-import { PersistentAuthenticator, createPersistentAuthenticatorStorage } from '@cardano-sdk/cip30';
-import { logger, walletName } from '../util';
-import { requestAccess } from './requestAccess';
-import { storage } from 'webextension-polyfill';
-
-const authenticatorStorage = createPersistentAuthenticatorStorage(`${walletName}Origins`, storage.local);
-
-export const authenticator = new PersistentAuthenticator({ requestAccess }, { logger, storage: authenticatorStorage });
--- packages/web-extension/e2e/extension/contentScript.ts
+++
@@ -1,8 +0,0 @@
-import { cip30 } from '@cardano-sdk/web-extension';
-import { runtime } from 'webextension-polyfill';
-import { walletName } from './util';
-
-cip30.initializeContentScript(
-  { injectedScriptSrc: runtime.getURL('injectedScript.js'), walletName },
-  { logger: console, runtime }
-);
