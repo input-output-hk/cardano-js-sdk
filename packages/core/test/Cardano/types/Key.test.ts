db1f42c9d21115e43a1581de077b5f4f9c84ca43
Tomislav Horaček <tomislav@tt-media.hr>
2022-07-08 14:50:22 +0200

    feat(core): added Ed25519PrivateKey to keys

--- packages/core/test/Cardano/types/Key.test.ts
+++ packages/core/test/Cardano/types/Key.test.ts
@@ -14,6 +14,21 @@ describe('Cardano/types/Key', () => {
     ).not.toThrow();
   });
 
+  it('Ed25519PrivateKey() accepts a valid private key hex string', () => {
+    expect(() =>
+      Cardano.Ed25519PrivateKey(
+        '6199186adb51974690d7247d2646097d2c62763b767b528816fb7ed3f9f55d396199186adb51974690d7247d2646097d2c62763b767b528816fb7ed3f9f55d39'
+      )
+    ).not.toThrow();
+    expect(() =>
+      Cardano.Ed25519PrivateKey.fromHexBlob(
+        Cardano.util.HexBlob(
+          '6199186adb51974690d7247d2646097d2c62763b767b528816fb7ed3f9f55d396199186adb51974690d7247d2646097d2c62763b767b528816fb7ed3f9f55d39'
+        )
+      )
+    ).not.toThrow();
+  });
+
   describe('Ed25519KeyHash', () => {
     it('accepts a key hash hex string', () => {
       expect(() => Cardano.Ed25519KeyHash('6199186adb51974690d7247d2646097d2c62763b767b528816fb7ed5')).not.toThrow();
