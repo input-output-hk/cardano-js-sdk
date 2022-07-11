fef65d0da9413a2f4631736240ddb88d1de7a86b
Mircea Hasegan <mircea.hasegan@iohk.io>
2022-07-11 14:05:59 +0200

    fix(wallet): remove assets no longer available in total balance
    BalanceTracker updates are emitted after metadata was fetched for all assets
    emitted by balance, instead of fetching one by one and emitting incremental Maps

--- packages/wallet/src/services/AssetsTracker.ts
+++ packages/wallet/src/services/AssetsTracker.ts
@@ -1,9 +1,9 @@
 import { Asset, Cardano } from '@cardano-sdk/core';
 import { BalanceTracker } from './types';
+import { Observable, forkJoin, map, mergeMap, of, tap } from 'rxjs';
 import { RetryBackoffConfig } from 'backoff-rxjs';
 import { TrackedAssetProvider } from './ProviderTracker';
 import { coldObservableProvider } from './util';
-import { distinct, from, map, mergeMap, of, scan, tap } from 'rxjs';
 
 export const createAssetService =
   (assetProvider: TrackedAssetProvider, retryBackoffConfig: RetryBackoffConfig) => (assetId: Cardano.AssetId) =>
@@ -28,11 +28,24 @@ export const createAssetsTracker = (
   { assetProvider, balanceTracker, retryBackoffConfig }: AssetsTrackerProps,
   { assetService = createAssetService(assetProvider, retryBackoffConfig) }: AssetsTrackerInternals = {}
 ) =>
-  balanceTracker.utxo.total$.pipe(
-    map(({ assets }) => [...(assets?.keys() || [])]),
-    tap((assetIds) => assetIds.length === 0 && assetProvider.setStatInitialized(assetProvider.stats.getAsset$)),
-    mergeMap((assetIds) => from(assetIds)),
-    distinct(),
-    mergeMap((assetId) => assetService(assetId)),
-    scan((assets, asset) => new Map([...assets, [asset.assetId, asset]]), new Map<Cardano.AssetId, Asset.AssetInfo>())
-  );
+  new Observable<Map<Cardano.AssetId, Asset.AssetInfo>>((subscriber) => {
+    let assetsMap = new Map<Cardano.AssetId, Asset.AssetInfo>();
+    const sub = balanceTracker.utxo.total$
+      .pipe(
+        map(({ assets }) => [...(assets?.keys() || [])]),
+        tap((assetIds) => assetIds.length === 0 && assetProvider.setStatInitialized(assetProvider.stats.getAsset$)),
+        // Fetch asset metadata only for assets not already present in assetsMap
+        map((assetIds) =>
+          assetIds.map((assetId) => (assetsMap.has(assetId) ? of(assetsMap.get(assetId)) : assetService(assetId)))
+        ),
+        // Wait for all asset metadata fetches to complete
+        mergeMap((assetInfos) => forkJoin(assetInfos)),
+        map((assetInfos) => new Map(assetInfos.map((assetInfo) => [assetInfo!.assetId, assetInfo!]))),
+        tap((v) => (assetsMap = v))
+      )
+      .subscribe(subscriber);
+
+    return () => {
+      sub.unsubscribe();
+    };
+  });
--- packages/wallet/test/services/AssetsTracker.test.ts
+++ packages/wallet/test/services/AssetsTracker.test.ts
@@ -1,9 +1,32 @@
 import { Asset, Cardano } from '@cardano-sdk/core';
 import { AssetId, createTestScheduler } from '@cardano-sdk/util-dev';
-import { AssetsTrackerProps, BalanceTracker, TransactionalTracker, createAssetsTracker } from '../../src/services';
+import {
+  AssetService,
+  AssetsTrackerProps,
+  BalanceTracker,
+  TrackedAssetProvider,
+  TransactionalTracker,
+  createAssetsTracker
+} from '../../src/services';
 import { of } from 'rxjs';
 
 describe('createAssetsTracker', () => {
+  let assetTsla: Asset.AssetInfo;
+  let assetPxl: Asset.AssetInfo;
+  let assetService: AssetService;
+  let assetProvider: TrackedAssetProvider;
+
+  beforeEach(() => {
+    const nftMetadata = { name: 'nft' } as Asset.NftMetadata;
+    assetTsla = { assetId: AssetId.TSLA } as Asset.AssetInfo;
+    assetPxl = { assetId: AssetId.PXL, nftMetadata } as Asset.AssetInfo;
+    assetService = jest.fn().mockReturnValueOnce(of(assetTsla)).mockReturnValueOnce(of(assetPxl));
+    assetProvider = {
+      setStatInitialized: jest.fn(),
+      stats: {}
+    } as unknown as TrackedAssetProvider;
+  });
+
   it('fetches asset info for every asset in total balance', () => {
     createTestScheduler().run(({ cold, expectObservable, flush }) => {
       const balanceTracker = {
@@ -20,26 +43,52 @@ describe('createAssetsTracker', () => {
           })
         }
       } as unknown as TransactionalTracker<BalanceTracker>;
-      const nftMetadata = { name: 'nft' } as Asset.NftMetadata;
-      const asset1 = { assetId: AssetId.TSLA } as Asset.AssetInfo;
-      const asset2 = { assetId: AssetId.PXL, nftMetadata } as Asset.AssetInfo;
-      const assetService = jest.fn().mockReturnValueOnce(of(asset1)).mockReturnValueOnce(of(asset2));
-      const assetProvider = {
-        setStatInitialized: jest.fn(),
-        stats: {}
-      };
+
       const target$ = createAssetsTracker({ assetProvider, balanceTracker } as unknown as AssetsTrackerProps, {
         assetService
       });
       expectObservable(target$).toBe('--b-c', {
-        b: new Map([[AssetId.TSLA, asset1]]),
+        b: new Map([[AssetId.TSLA, assetTsla]]),
         c: new Map([
-          [AssetId.TSLA, asset1],
-          [AssetId.PXL, asset2]
+          [AssetId.TSLA, assetTsla],
+          [AssetId.PXL, assetPxl]
         ])
       });
       flush();
       expect(assetProvider.setStatInitialized).toBeCalledTimes(1); // only when there are no assets
+      expect(assetService).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  it('removes assets no longer available in total balance', () => {
+    createTestScheduler().run(({ cold, expectObservable, flush }) => {
+      const balanceTracker = {
+        utxo: {
+          total$: cold('a-b-c', {
+            a: {} as Cardano.Value,
+            b: {
+              assets: new Map([
+                [AssetId.TSLA, 1n],
+                [AssetId.PXL, 2n]
+              ])
+            } as Cardano.Value,
+            c: { assets: new Map([[AssetId.PXL, 2n]]) } as Cardano.Value
+          })
+        }
+      } as unknown as TransactionalTracker<BalanceTracker>;
+
+      const target$ = createAssetsTracker({ assetProvider, balanceTracker } as unknown as AssetsTrackerProps, {
+        assetService
+      });
+      expectObservable(target$).toBe('--b-c', {
+        b: new Map([
+          [AssetId.TSLA, assetTsla],
+          [AssetId.PXL, assetPxl]
+        ]),
+        c: new Map([[AssetId.PXL, assetPxl]])
+      });
+      flush();
+      expect(assetService).toHaveBeenCalledTimes(2);
     });
   });
 });
