3c547bc326261465a1fdbd73ea56b996cb31dc52
David Arnold <david.arnold@iohk.io>
2022-07-12 15:54:49 -0500

    run yarn

--- flake.nix
+++ flake.nix
@@ -1,19 +1,9 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = inputs: let
-    nixpkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    inherit (nixpkgs) lib;
-  in rec {
-    packages.x86_64-linux = nixpkgs.yarn2nix-moretea.mkYarnWorkspace {
-      src = ./.;
-    };
-    checks.x86_64-linux = lib.filterAttrs (k: v: lib.hasPrefix "@cardano-sdk/" k) packages.x86_64-linux;
-    devShells.x86_64-linux.default = nixpkgs.mkShell {
-      nativeBuildInputs = [
-        nixpkgs.nodejs-16_x.pkgs.yarn
-        nixpkgs.nodejs-16_x
-      ];
-    };
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.mapAttrs (system: pkgs: {
+      default = pkgs.callPackage ./yarn-project.nix { } { src = ./.; };
+    }) nixpkgs.legacyPackages;
   };
 }
