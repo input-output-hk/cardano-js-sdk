8a5ec35cd1af283b15a494d8b25911543252d1b8
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-07-13 15:18:07 +0300

    feat(wallet): add createLazyWalletUtil
    Workaround for circular dependency of:
    WalletUtil->SingleAddressWallet->KeyAgent->WalletUtil

--- packages/wallet/test/services/WalletUtil.test.ts
+++ packages/wallet/test/services/WalletUtil.test.ts
@@ -3,7 +3,9 @@ import { Cardano } from '@cardano-sdk/core';
 import {
   OutputValidator,
   ProtocolParametersRequiredByOutputValidator,
+  WalletUtilContext,
   createInputResolver,
+  createLazyWalletUtil,
   createOutputValidator
 } from '../../src';
 import { of } from 'rxjs';
@@ -81,6 +83,21 @@ describe('WalletUtil', () => {
           txId: Cardano.TransactionId('0f3abbc8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56fe0e78f19d9d4')
         })
       ).toBeNull();
+    });
+  });
+
+  describe('createLazyWalletUtil', () => {
+    it('awaits for "initialize" to be called before resolving call to any util', async () => {
+      const util = createLazyWalletUtil();
+      const resultPromise = util.validateValue({ coins: 2_000_000n });
+      util.initialize({
+        protocolParameters$: of<ProtocolParametersRequiredByOutputValidator>({
+          coinsPerUtxoByte: 4310,
+          maxValueSize: 90
+        })
+      } as WalletUtilContext);
+      const result = await resultPromise;
+      expect(result.coinMissing).toBe(0n);
     });
   });
 });
