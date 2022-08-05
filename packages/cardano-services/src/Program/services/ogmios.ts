f323f0285107c5d8dc906a44f1d34608b1c4b0b9
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2022-08-05 15:19:50 +1000

    docs(cardano-services): fix spelling/grammar mistakes

--- packages/cardano-services/src/Program/services/ogmios.ts
+++ packages/cardano-services/src/Program/services/ogmios.ts
@@ -28,12 +28,12 @@ const recreateOgmiosCardanoNode = async (
 };
 
 /**
- * Creates a extended TxSubmitProvider instance :
+ * Creates an extended TxSubmitProvider instance :
  * - use passed srv service name in order to resolve the port
- * - make dealing with failovers (re-resolving the port) opaque
+ * - make dealing with fail-overs (re-resolving the port) opaque
  * - use exponential backoff retry internally with default timeout and factor
  * - intercept 'submitTx' operation and handle connection errors runtime
- * - all other operations are bind to pool object withoud modifications
+ * - all other operations are bind to pool object without modifications
  *
  * @returns TxSubmitProvider instance
  */
@@ -89,13 +89,13 @@ export const getOgmiosTxSubmitProvider = async (
 };
 
 /**
- * Creates a extended OgmiosCardanoNode instance :
+ * Creates an extended OgmiosCardanoNode instance :
  * - use passed srv service name in order to resolve the port
- * - make dealing with failovers (re-resolving the port) opaque
+ * - make dealing with fail-overs (re-resolving the port) opaque
  * - use exponential backoff retry internally with default timeout and factor
  * - intercept 'initialize' operation and handle connection errors on initialization
  * - intercept 'eraSummaries' operation and handle connection errors runtime
- * - all other operations are bind to pool object withoud modifications
+ * - all other operations are bind to pool object without modifications
  *
  * @returns OgmiosCardanoNode instance
  */
