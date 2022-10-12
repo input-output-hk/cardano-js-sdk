9d8f9b5d94515a4fd723b35a1d96a94fe374a209
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-10-12 10:48:03 +0300

    chore(core): add console.log to parseCslAddress catch

--- packages/core/src/CSL/parseCslAddress.ts
+++ packages/core/src/CSL/parseCslAddress.ts
@@ -6,7 +6,9 @@ import { CSL } from './CSL';
 export const parseCslAddress = (input: string): CSL.Address | null => {
   try {
     return CSL.Address.from_bech32(input);
-  } catch {
+  } catch (error: unknown) {
+    // eslint-disable-next-line no-console
+    console.error('CSL failed to parse address', `"${input}"`, error);
     try {
       return CSL.ByronAddress.from_base58(input).to_address();
     } catch {
