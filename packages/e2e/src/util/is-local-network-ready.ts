38893bcc7e5837a0c6a21ad47007b9e5f192d14b
Angel Castillo <Angel.Castillo@iohk.io>
2022-10-17 10:08:44 +0800

    test: split continuous integration in two workflows, e2e and unit tests
    (cherry picked from commit 97dec49efd0447a3d21d20668534b92537483baf)

---
+++ packages/e2e/src/util/is-local-network-ready.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import * as Process from 'process';
+import { Pool, QueryResult } from 'pg';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Waits until the local network is ready or the wait time expires.
+ */
+(async () => {
+  if (Process.env.DB_SYNC_CONNECTION_STRING === undefined) {
+    console.error('The DB_SYNC_CONNECTION_STRING env variable must be defined');
+    return -1;
+  }
+
+  const start = Date.now() / 1000;
+  const waitTime = Process.env.LOCAL_NETWORK_READY_WAIT_TIME ? Process.env.LOCAL_NETWORK_READY_WAIT_TIME : 1200;
+  const db: Pool = new Pool({ connectionString: Process.env.DB_SYNC_CONNECTION_STRING });
+  let isReady = false;
+  let currentElapsed = 0;
+
+  while (!isReady && currentElapsed < waitTime) {
+    try {
+      console.log('Waiting...');
+      const inputsResults: QueryResult<{ no: number }> = await db.query(
+        'select epoch.no from epoch order by epoch.no DESC limit 1'
+      );
+
+      isReady = inputsResults.rows[0].no >= 4; // One more than the local network healthcheck
+    } catch {
+      // continue
+    } finally {
+      currentElapsed = Date.now() / 1000 - start;
+      await sleep(5000);
+    }
+  }
+
+  if (currentElapsed > waitTime) {
+    console.error('Wait time expired. The local test network was not started on time.');
+    return -1;
+  }
+
+  console.log('Local network ready!');
+})();
