65d78fc015bf7bd856c5febe0ba84d3ad18a069c
Gytis Ivaskevicius <me@gytis.io>
2022-10-28 13:51:38 +0300

    fix(nix): fix not found /packages/cardano-services/node_modules

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
             cp -r packages/$p/dist $out/libexec/$sourceRoot/packages/$p/dist
             cp -r packages/$p/package.json $out/libexec/$sourceRoot/packages/$p/package.json
           done
-          cp -r ${production-deps}/libexec/$sourceRoot/packages/cardano-services/node_modules $out/libexec/$sourceRoot/packages/cardano-services/node_modules
+          test -f ${production-deps}/libexec/$sourceRoot/packages/cardano-services/node_modules && cp -r ${production-deps}/libexec/$sourceRoot/packages/cardano-services/node_modules $out/libexec/$sourceRoot/packages/cardano-services/node_modules
           cp -r ${production-deps}/libexec/$sourceRoot/packages/cardano-services/config $out/libexec/$sourceRoot/packages/cardano-services/config
 
           cd "$out/libexec/$sourceRoot"
