c48f6d4eba896861b6e35ff39571bc261bafa991
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2022-11-15 17:07:40 +0200

    feat(ogmios): add ogmiosToCore.genesis

---
+++ packages/ogmios/test/ogmiosToCore/genesis.test.ts
@@ -0,0 +1,30 @@
+import { Cardano } from '@cardano-sdk/core';
+import { Schema } from '@cardano-ogmios/client';
+import { ogmiosToCore } from '../../src';
+
+describe('ogmiosToCore', () => {
+  describe('genesis', () => {
+    it('converts all incompatible genesis properties to core types', () => {
+      const ogmiosGenesis: Omit<Schema.CompactGenesis, 'protocolParameters'> = {
+        activeSlotsCoefficient: '1/20',
+        epochLength: 86_400,
+        maxKesEvolutions: 120,
+        maxLovelaceSupply: 45_000_000_000_000_000,
+        network: 'testnet',
+        networkMagic: 2,
+        securityParameter: 432,
+        slotLength: 1,
+        slotsPerKesPeriod: 86_400,
+        systemStart: '2022-08-09T00:00:00Z',
+        updateQuorum: 5
+      };
+      expect(ogmiosToCore.genesis(ogmiosGenesis as Schema.CompactGenesis)).toEqual({
+        ...ogmiosGenesis,
+        activeSlotsCoefficient: 0.05,
+        maxLovelaceSupply: 45_000_000_000_000_000n,
+        networkId: Cardano.NetworkId.testnet,
+        systemStart: new Date('2022-08-09T00:00:00Z')
+      });
+    });
+  });
+});
