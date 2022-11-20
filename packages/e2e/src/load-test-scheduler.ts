d138127cf05b6c4aaf02629725aca28686264fe2
Mircea Hasegan <mircea.hasegan@iohk.io>
2022-11-20 10:24:05 +0100

    test(e2e): wallet init test with new custom measurement and scheduler helpers
    Added `MeasurementUtil` helper class based on node perf hooks.
    Added `getLoadTestScheduler` util that schedules a given number of calls in a timeframe.
    Added wallet initialization load test using the two utils.

---
+++ packages/e2e/src/load-test-scheduler.ts
@@ -0,0 +1,35 @@
+import { Logger } from 'ts-log';
+import { Observable, map, mergeMap, take, timer } from 'rxjs';
+
+export interface LoadTestSchedulerProps<T> {
+  /** Time in seconds during which the scheduler calls the `callUnderTest` */
+  duration: number;
+  /** How many times scheduler will call `callUnderTest` in the given `duration` */
+  callsPerDuration: number;
+  /** Each call will have a unique incrementing numeric id. This is the start value. */
+  idOffset?: number;
+  /** Asynchronous call that will be called `callsPerDuration` times, evenly split in `duration` seconds */
+  callUnderTest: (id: number) => Observable<T>;
+}
+
+export interface LoadTestSchedulerDependencies {
+  logger: Logger;
+}
+
+/** Use `LoadTestSchedulerProps` to schedule calls to `callUnderTest` */
+export const getLoadTestScheduler = <T>(
+  { duration, callsPerDuration, idOffset = 0, callUnderTest }: LoadTestSchedulerProps<T>,
+  { logger }: LoadTestSchedulerDependencies
+): Observable<T> =>
+  new Observable<T>((observer) => {
+    const callIntervalMs = (duration * 1000) / callsPerDuration;
+    logger.info(`Running ${callsPerDuration} calls in ${duration}s, 1 call / ${callIntervalMs}ms`);
+
+    return timer(0, callIntervalMs)
+      .pipe(
+        map((id) => id + idOffset),
+        take(callsPerDuration),
+        mergeMap((id) => callUnderTest(id))
+      )
+      .subscribe(observer);
+  });
