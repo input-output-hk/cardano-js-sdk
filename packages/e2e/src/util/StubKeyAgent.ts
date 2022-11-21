0c89bad3d68bc8f754b3956aa046b16a5ddb6c04
Ivaylo Andonov <ivaylo.andonov@iohk.io>
2022-11-21 23:15:42 +0000

    test(e2e): wallet restoration load test - use artillery as load test engine - add wallet restoration scenario - update readme with usage

---
+++ packages/e2e/src/util/StubKeyAgent.ts
@@ -0,0 +1,61 @@
+import {
+  AccountAddressDerivationPath,
+  AccountKeyDerivationPath,
+  GroupedAddress,
+  KeyAgent,
+  SerializableKeyAgentData,
+  SignBlobResult,
+  SignTransactionOptions
+} from '@cardano-sdk/key-management';
+import { Cardano, NotImplementedError } from '@cardano-sdk/core';
+
+export class StubKeyAgent implements KeyAgent {
+  readonly #knownAddresses: GroupedAddress[];
+
+  constructor(groupedAddress: GroupedAddress) {
+    this.#knownAddresses = [groupedAddress];
+  }
+
+  get knownAddresses(): GroupedAddress[] {
+    return this.#knownAddresses;
+  }
+
+  get networkId(): Cardano.NetworkId {
+    throw new NotImplementedError('networkId');
+  }
+
+  get accountIndex(): number {
+    throw new NotImplementedError('accountIndex');
+  }
+
+  get serializableData(): SerializableKeyAgentData {
+    throw new NotImplementedError('serializableData');
+  }
+
+  get extendedAccountPublicKey(): Cardano.Bip32PublicKey {
+    throw new NotImplementedError('extendedAccountPublicKey');
+  }
+
+  deriveAddress(_derivationPath: AccountAddressDerivationPath): Promise<GroupedAddress> {
+    throw new NotImplementedError('deriveAddress');
+  }
+
+  derivePublicKey(_derivationPath: AccountKeyDerivationPath): Promise<Cardano.Ed25519PublicKey> {
+    throw new NotImplementedError('derivePublicKey');
+  }
+
+  signBlob(_derivationPath: AccountKeyDerivationPath, _blob: Cardano.util.HexBlob): Promise<SignBlobResult> {
+    throw new NotImplementedError('signBlob');
+  }
+
+  signTransaction(
+    _txInternals: Cardano.TxBodyWithHash,
+    _options?: SignTransactionOptions | undefined
+  ): Promise<Cardano.Signatures> {
+    throw new NotImplementedError('signTransaction');
+  }
+
+  exportRootPrivateKey(): Promise<Cardano.Bip32PrivateKey> {
+    throw new NotImplementedError('exportRootPrivateKey');
+  }
+}
---
+++ packages/e2e/test/artillery/wallet-restoration/queries.ts
@@ -0,0 +1,14 @@
+/**
+ * Query randomized distinct addresses from db associated with users who are staking
+ */
+export const findAddressesWithRewardsHistory = `
+  SELECT * FROM (
+    SELECT DISTINCT txOut.address as address, sa.view as stake_address
+    FROM public.delegation d
+      LEFT JOIN public.tx_out txOut on
+    d.addr_id = txOut.stake_address_id
+      LEFT JOIN public.stake_address sa on
+    txOut.stake_address_id = sa.id
+  ) distinct_addresses
+  ORDER BY RANDOM() LIMIT $1
+`;
