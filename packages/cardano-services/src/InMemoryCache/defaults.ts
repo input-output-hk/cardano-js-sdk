f0eb80f73e61ea48f10809fb3c329fb5c4022e6b
Daniele Ricci <daniele.ricci@iohk.io>
2022-11-25 07:13:32 +0100

    fix!: ttl validation now uses seconds and no longer minutes as the cache itself
    BREAKING CHANGE: Even if the value of DB_CACHE_TTL was already interpreted in seconds
    rather than in minutes (despite the error in the description), it's default value is
    changed

--- packages/cardano-services/src/InMemoryCache/defaults.ts
+++ packages/cardano-services/src/InMemoryCache/defaults.ts
@@ -1,5 +1,5 @@
 export const DB_CACHE_TTL_DEFAULT = 2 * 60 * 60; // Two hours
 
-export const CACHE_TTL_LOWER_LIMIT = 1;
-export const CACHE_TTL_UPPER_LIMIT = 2880;
+export const CACHE_TTL_LOWER_LIMIT = 60; // One minute
+export const CACHE_TTL_UPPER_LIMIT = 2 * 24 * 60 * 60; // Two days
 export const UNLIMITED_CACHE_TTL = 0;
