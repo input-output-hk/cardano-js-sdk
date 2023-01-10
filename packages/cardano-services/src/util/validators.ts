e9c3a5b74a2cc1f69c53f562af8a5b4e693bd20a
Ivaylo Andonov <ivaylo.andonov@iohk.io>
2023-01-10 12:43:51 +0000

    feat(cardano-services)!: upgrade /meta endpoint - introduce ServerMetadata type - add `--build-info` cli arg and BUILD_INFO env - add metadata json validation on startup - add the endpoint to OpenApi def
    BREAKING CHANGE:
    
    Nested `narHash`, `path` and `sourceInfo` under top level `extra` property of BUILD_INFO

--- packages/cardano-services/src/util/validators.ts
+++ packages/cardano-services/src/util/validators.ts
@@ -1,5 +1,38 @@
+import { BuildInfo } from '../Http';
 import { CACHE_TTL_LOWER_LIMIT, CACHE_TTL_UPPER_LIMIT } from '../InMemoryCache';
 import { MissingProgramOption, ProgramOptionDescriptions, ServiceNames } from '../Program';
+import { validate } from 'jsonschema';
+import fs from 'fs';
+
+const buildInfoSchema = {
+  additionalProperties: false,
+  properties: {
+    extra: { type: 'object' },
+    lastModified: { type: 'number' },
+    lastModifiedDate: { type: 'string' },
+    rev: { type: 'string' },
+    shortRev: { type: 'string' }
+  },
+  type: 'object'
+};
+
+export const buildInfoValidator = (buildInfo: string): BuildInfo => {
+  let result: BuildInfo;
+  try {
+    result = JSON.parse(buildInfo || '{}');
+  } catch (error) {
+    throw new Error(`Invalid JSON format of process.env.BUILD_INFO: ${error}`);
+  }
+  validate(result, buildInfoSchema, { throwError: true });
+  return result;
+};
+
+export const existingFileValidator = (filePath: string) => {
+  if (fs.existsSync(filePath)) {
+    return filePath;
+  }
+  throw new Error(`No file exists at ${filePath}`);
+};
 
 export const cacheTtlValidator = (ttl: string) => {
   const cacheTtl = Number.parseInt(ttl, 10);
