2757ce5ed79dd15b98dd4548878a39918d56dc5a
wklos-iohk <wojciech.klos@iohk.io>
2023-02-15 15:31:39 +0100

    test(mobile): adjust detox config

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -1,12 +1,11 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 
-const untranspiledModulePatterns = ['react-native', '@react-native*'];
-
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    `node_modules/.pnpm/(?!${untranspiledModulePatterns.join('|')})`,
+    'node_modules',
+    'jest-runner',
   ],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/e2e/'],
 };
