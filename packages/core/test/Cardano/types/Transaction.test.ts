f408baa25b330134f1502e460eb41ec2d46dcb58
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-02-15 14:26:40 +0200

    feat(core): add OpaqueString types for TxCBOR and TxBodyCBOR with some utility functions
    add TransactionId.fromTxBodyCbor util

--- packages/core/test/Cardano/types/Transaction.test.ts
+++ packages/core/test/Cardano/types/Transaction.test.ts
@@ -1,15 +1,31 @@
-import { Cardano } from '../../../';
+import { Cardano, TxBodyCBOR, TxCBOR } from '../../../';
 import { Ed25519SignatureHex } from '@cardano-sdk/crypto';
 import { HexBlob } from '@cardano-sdk/util';
+import { babbageTx } from '../../CML/testData';
 
 describe('Cardano/types/Transaction', () => {
-  it('TransactionId accepts a valid transaction hash hex string', () => {
-    expect(() =>
-      Cardano.TransactionId('3e33018e8293d319ef5b3ac72366dd28006bd315b715f7e7cfcbd3004129b80d')
-    ).not.toThrow();
-    expect(() =>
-      Cardano.TransactionId.fromHexBlob(HexBlob('3e33018e8293d319ef5b3ac72366dd28006bd315b715f7e7cfcbd3004129b80d'))
-    ).not.toThrow();
+  describe('TransactionId', () => {
+    describe('when used as a constructor', () => {
+      it('TransactionId accepts a valid transaction hash hex string', () => {
+        expect(() =>
+          Cardano.TransactionId('3e33018e8293d319ef5b3ac72366dd28006bd315b715f7e7cfcbd3004129b80d')
+        ).not.toThrow();
+        expect(() =>
+          Cardano.TransactionId.fromHexBlob(HexBlob('3e33018e8293d319ef5b3ac72366dd28006bd315b715f7e7cfcbd3004129b80d'))
+        ).not.toThrow();
+      });
+    });
+
+    describe('TransactionId.fromTxBodyCbor', () => {
+      it('computes transaction hash', () => {
+        const bodyCbor = TxBodyCBOR.fromTxCBOR(TxCBOR.serialize(babbageTx));
+        const txId = Cardano.TransactionId.fromTxBodyCbor(bodyCbor);
+        expect(typeof txId).toBe('string');
+        expect(txId).toHaveLength(64);
+        // hex characters only
+        expect(Buffer.from(txId, 'hex').toString('hex')).toEqual(txId);
+      });
+    });
   });
 
   it('Ed25519Signature() accepts a valid signature hex string', () => {
