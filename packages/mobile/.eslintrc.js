a1a963f00546bd991693fee88908d3b1478a98a7
wklos-iohk <wojciech.klos@iohk.io>
2023-02-16 09:09:28 +0100

    test(mobile): update .eslintrc.js

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'detox'],
+  plugins: ['detox'],
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
