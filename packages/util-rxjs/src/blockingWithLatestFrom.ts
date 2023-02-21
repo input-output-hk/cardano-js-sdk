ecea3702955a0e8969f00ca64c4f256168ec0e60
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-02-21 13:09:36 +0200

    fix(util-rxjs): set blockingWithLatestFrom mergeScan concurrency to 1
    Unlimited concurrency can mess up the order of source values:
    if source emits during emission of from(buffer), then the new
    item from source can end up being emitted in-between buffer items.

--- packages/util-rxjs/src/blockingWithLatestFrom.ts
+++ packages/util-rxjs/src/blockingWithLatestFrom.ts
@@ -50,24 +50,40 @@ export function blockingWithLatestFrom<T, R>(
         takeUntil(sharedSource$.pipe(defaultIfEmpty(null), delay(1), last()))
       )
     ]).pipe(
-      mergeScan(({ buffer }: Accumulator, output): Observable<Accumulator> => {
-        // add source values to buffer until dependency$ emits
-        if (output[1] === EMPTY_DEPENDENCY) {
+      mergeScan(
+        ({ buffer }: Accumulator, output): Observable<Accumulator> => {
+          // add source values to buffer until dependency$ emits
+          if (output[1] === EMPTY_DEPENDENCY) {
+            return of({
+              buffer: [...buffer, output[0]],
+              output: null
+            });
+          }
+          if (buffer.length > 0) {
+            const lastItem = buffer[buffer.length - 1];
+            const items = lastItem === output[0] ? buffer : [...buffer, output[0]];
+            // emit values from buffer and the current value
+            return from(
+              items.map((sourceValue) => ({
+                buffer: [],
+                output: [sourceValue, output[1]] as [T, any]
+              }))
+            );
+          }
           return of({
-            buffer: [...buffer, output[0]],
-            output: null
-          });
-        }
-        // emit values from buffer and the current value
-        return from(
-          [...buffer, output[0]].map((sourceValue) => ({
             buffer: [],
-            output: [sourceValue, output[1]] as [T, any]
-          }))
-        );
-      }, accumulator),
+            output
+          });
+        },
+        accumulator,
+        // Unlimited concurrency can mess up the order of source values:
+        // if source emits during emission of from(buffer), then the new
+        // item from source can end up being emitted in-between buffer items.
+        1
+      ),
       map(({ output }) => output),
       filter(isNotNil),
+      // only emitting when source item changes
       distinctUntilKeyChanged(0),
       map(([sourceValue, dependency]) => combinator(sourceValue, dependency))
     );
