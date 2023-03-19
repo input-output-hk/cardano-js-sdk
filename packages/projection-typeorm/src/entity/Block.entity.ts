64b6846193126bd7d41345ec6f10017296500373
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-03-19 10:10:42 +0200

    refactor(projection-typeorm): use opaque types for Block entity props

--- packages/projection-typeorm/src/entity/Block.entity.ts
+++ packages/projection-typeorm/src/entity/Block.entity.ts
@@ -4,13 +4,13 @@ import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
 @Entity()
 export class BlockEntity {
   @PrimaryColumn()
-  height?: number;
+  height?: Cardano.BlockNo;
 
   @Index({ unique: true })
   @Column({ length: 64, type: 'char' })
   hash?: Cardano.BlockId;
 
   @Index({ unique: true })
-  @Column({ type: 'bigint' })
-  slot?: number;
+  @Column()
+  slot?: Cardano.Slot;
 }
