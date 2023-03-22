db58ad4fca9ba9257375bd20e43c74a6b2a8cf39
Daniele Ricci <daniele.ricci@iohk.io>
2023-03-22 18:01:54 +0100

    feat(util): adds an util to resolve all promises contained within an object

---
+++ packages/util/test/util.test.ts
@@ -0,0 +1,15 @@
+import { resolveObjectValues } from '../src';
+
+describe('util', () => {
+  describe('resolveObjectValues', () => {
+    it('resolves all object values which are promises', async () => {
+      const result = await resolveObjectValues({
+        first: 1,
+        second: Promise.resolve(2),
+        third: new Promise<number>((resolve) => setTimeout(() => resolve(3), 10))
+      });
+
+      expect(result).toEqual({ first: 1, second: 2, third: 3 });
+    });
+  });
+});
