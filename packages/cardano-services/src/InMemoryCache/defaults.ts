499d8200b58747e7a68f771affdff311e70deef0
Dmytro Iakymenko <dmytro.iakymenko@iohk.io>
2023-03-29 18:45:12 +0300

    fix(cardano-services): unlimited_cache_ttl

--- packages/cardano-services/src/InMemoryCache/defaults.ts
+++ packages/cardano-services/src/InMemoryCache/defaults.ts
@@ -2,4 +2,4 @@ export const DB_CACHE_TTL_DEFAULT = 2 * 60 * 60; // Two hours
 
 export const CACHE_TTL_LOWER_LIMIT = 60; // One minute
 export const CACHE_TTL_UPPER_LIMIT = 2 * 24 * 60 * 60; // Two days
-export const UNLIMITED_CACHE_TTL = 0;
+export const UNLIMITED_CACHE_TTL = undefined;
