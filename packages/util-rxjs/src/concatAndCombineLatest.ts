305f2ed0039c8f355e4b973c41392c8c601b9312
Mircea Hasegan <mircea.hasegan@iohk.io>
2023-04-12 15:18:12 +0200

    feat(wallet): concatAndCombineLatest creator function

---
+++ packages/util-rxjs/src/concatAndCombineLatest.ts
@@ -0,0 +1,32 @@
+import { Observable, combineLatest, concat, ignoreElements, of, share, take } from 'rxjs';
+
+/** Subscribe to o$ after trigger$ emits its first value */
+const startOnTrigger = <A, B>(trigger$: Observable<A>, o$: Observable<B>) =>
+  concat(trigger$.pipe(take(1), ignoreElements()), o$);
+
+/**
+ * Creates a new observable that:
+ *   - acts like `combineLatest` for the input `args` observables
+ *   - unlike `combineLatest` it starts the observables in a sequence, like concat
+ *   - unlike `concat`, it does not wait for the previous one to complete,
+ *     instead it starts the next observable when the previous one emits the first value
+ */
+export const concatAndCombineLatest = <T>(obsArray: Observable<T>[]): Observable<T[]> => {
+  if (obsArray.length === 0) {
+    return of([]);
+  }
+
+  // Build an array of observables, where each item in the array starts only when the previous one emitted the first value
+  const observableCascadeArray: Observable<T>[] = [];
+  // Share the observable to be used as source and as trigger
+  let sharedTrigger$ = obsArray[0].pipe(share());
+  observableCascadeArray.push(sharedTrigger$);
+  for (const o$ of obsArray.slice(1)) {
+    // next o$ starts only after sharedTrigger$ emitted once
+    const triggeredO$ = startOnTrigger(sharedTrigger$, o$);
+    // triggerO$ will be a source and a trigger for the next one, thus cascading the observables
+    sharedTrigger$ = triggeredO$.pipe(share());
+    observableCascadeArray.push(sharedTrigger$);
+  }
+  return combineLatest(observableCascadeArray);
+};
---
+++ packages/util-rxjs/test/concatAndCombineLatest.test.ts
@@ -0,0 +1,48 @@
+import { concatAndCombineLatest } from '../src';
+import { createTestScheduler } from '@cardano-sdk/util-dev';
+
+describe('concatAndCombineLatest', () => {
+  it('emits an empty array if the source is an empty array', () => {
+    createTestScheduler().run(({ expectObservable }) => {
+      expectObservable(concatAndCombineLatest([])).toBe('(a|)', { a: [] });
+    });
+  });
+
+  it('mirrors the values wrapped in array, given an array with a single observable', () => {
+    createTestScheduler().run(({ cold, expectObservable }) => {
+      const o$ = cold('xy|');
+      expectObservable(concatAndCombineLatest([o$])).toBe('ab|', { a: ['x'], b: ['y'] });
+    });
+  });
+
+  it('concats the emitted values for two observables', () => {
+    createTestScheduler().run(({ cold, expectObservable }) => {
+      const o1$ = cold('x|');
+      const o2$ = cold('y|');
+      expectObservable(concatAndCombineLatest([o1$, o2$])).toBe('a|', { a: ['x', 'y'] });
+    });
+  });
+
+  it(`for multiple observables,
+      subscribes sequentially like concat,
+      based on prev emitting at least once, instead of complete event,
+      and combines them like combineLatest
+      `, () => {
+    createTestScheduler().run(({ cold, expectObservable, expectSubscriptions }) => {
+      const o1$ = cold('-a--b--c|');
+      const o2$ = cold(' ---x-y--z|');
+      const o3$ = cold('    -m|');
+
+      expectObservable(concatAndCombineLatest([o1$, o2$, o3$])).toBe('-----abc-d|', {
+        a: ['b', 'x', 'm'],
+        b: ['b', 'y', 'm'],
+        c: ['c', 'y', 'm'],
+        d: ['c', 'z', 'm']
+      });
+
+      expectSubscriptions(o1$.subscriptions).toBe('^-------!');
+      expectSubscriptions(o2$.subscriptions).toBe('-^--------!');
+      expectSubscriptions(o3$.subscriptions).toBe('----^-!');
+    });
+  });
+});
