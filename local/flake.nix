221d4b18be6f04e96979bde2585e00a7064de976
David Arnold <david.arnold@iohk.io>
2023-05-02 11:59:06 -0500

    env: add minimal, reproducible dev env

---
+++ local/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Local Development Environment";
+  inputs.nosys.url = "github:divnix/nosys";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-compat.url = "github:edolstra/flake-compat?ref=refs/pull/55/head";
+  outputs = inputs @ {
+    nosys,
+    flake-compat,
+    ...
+  }:
+    nosys ((flake-compat ../.).inputs // inputs) (
+      {
+        self,
+        nixpkgs,
+        devshell,
+        ...
+      }:
+        with nixpkgs.legacyPackages;
+        with nixpkgs.legacyPackages.nodePackages;
+        with devshell.legacyPackages; let
+          inherit (lib.stringsWithDeps) noDepEntry;
+          checkMod = {
+            commands = [{package = treefmt;}];
+            packages = [alejandra shfmt prettier prettier-plugin-toml];
+            devshell.startup.nodejs-setuphook = noDepEntry ''
+              export NODE_PATH=${prettier-plugin-toml}/lib/node_modules:$NODE_PATH
+            '';
+          };
+        in {
+          devShells = {
+            check = mkShell {
+              imports = [checkMod];
+            };
+            default = mkShell {
+              name = "Hello My Friend";
+              imports = [checkMod];
+              commands = [
+                # {
+                #   package = cocogitto;
+                #   name = "cog";
+                # }
+              ];
+            };
+          };
+        }
+    );
+}
