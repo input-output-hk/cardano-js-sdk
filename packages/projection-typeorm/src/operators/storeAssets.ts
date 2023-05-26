ee8890cac599e38ddb7871f0e770e29e2de9a976
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-05-26 17:39:30 +0300

    feat: update storeAssets operator to return the new total supply for minted assets
    - update with-mint.json test data to add a 2nd mint transaction for an asset.
    - improve 'typeormOperator' util types to better support returned props

--- packages/projection-typeorm/src/operators/storeAssets.ts
+++ packages/projection-typeorm/src/operators/storeAssets.ts
@@ -1,26 +1,49 @@
 import { AssetEntity } from '../entity/Asset.entity';
-import { ChainSyncEventType } from '@cardano-sdk/core';
+import { Cardano, ChainSyncEventType } from '@cardano-sdk/core';
 import { Mappers } from '@cardano-sdk/projection';
 import { typeormOperator } from './util';
 
-export const storeAssets = typeormOperator<Mappers.WithMint>(
+type MintedAssetSupplies = Partial<Record<Cardano.AssetId, bigint>>;
+export type WithMintedAssetSupplies = {
+  mintedAssetTotalSupplies: MintedAssetSupplies;
+};
+
+export const storeAssets = typeormOperator<Mappers.WithMint, WithMintedAssetSupplies>(
+  // TODO: refactor
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   async ({ mint, block: { header }, eventType, queryRunner }) => {
     const repository = queryRunner.manager.getRepository(AssetEntity);
+    const mintedAssetTotalSupplies: MintedAssetSupplies = {};
     if (eventType === ChainSyncEventType.RollForward) {
       for (const { assetId, quantity } of mint) {
         const storedAsset = await repository.findOne({ select: { supply: true }, where: { id: assetId } });
-        await (storedAsset
-          ? repository.increment({ id: assetId }, 'supply', quantity.toString())
-          : repository.insert({
-              firstMintBlock: { slot: header.slot },
-              id: assetId,
-              supply: quantity
-            }));
+        if (storedAsset) {
+          const newSupply = storedAsset.supply! + quantity;
+          await repository.update({ id: assetId }, { supply: newSupply });
+          mintedAssetTotalSupplies[assetId] = newSupply;
+        } else {
+          await repository.insert({
+            firstMintBlock: { slot: header.slot },
+            id: assetId,
+            supply: quantity
+          });
+          mintedAssetTotalSupplies[assetId] = quantity;
+        }
       }
     } else {
       for (const { assetId, quantity } of mint) {
-        await repository.increment({ id: assetId }, 'supply', (-quantity).toString());
+        const isPositiveQuantity = quantity > 0n;
+        const absQuantity = isPositiveQuantity ? quantity : -1n * quantity;
+        const queryResponse = await queryRunner.manager
+          .createQueryBuilder(AssetEntity, 'asset')
+          .update()
+          .set({ supply: () => `supply ${isPositiveQuantity ? '-' : '+'} ${absQuantity}` })
+          .where({ id: assetId })
+          .returning(['supply'])
+          .execute();
+        mintedAssetTotalSupplies[assetId] = queryResponse.affected === 0 ? 0n : BigInt(queryResponse.raw[0].supply);
       }
     }
+    return { mintedAssetTotalSupplies };
   }
 );
--- packages/projection-typeorm/test/operators/storeAssets.test.ts
+++ packages/projection-typeorm/test/operators/storeAssets.test.ts
@@ -9,17 +9,18 @@ import {
   withTypeormTransaction
 } from '../../src';
 import { Bootstrap, Mappers, requestNext } from '@cardano-sdk/projection';
+import { Cardano, ChainSyncEventType } from '@cardano-sdk/core';
 import { ChainSyncDataSet, chainSyncData, logger } from '@cardano-sdk/util-dev';
-import { ChainSyncEventType } from '@cardano-sdk/core';
-import { QueryRunner } from 'typeorm';
+import { DataSource, QueryRunner } from 'typeorm';
+import { Observable, of } from 'rxjs';
 import { createProjectorTilFirst } from './util';
-import { defer, from } from 'rxjs';
 import { initializeDataSource } from '../util';
 
 describe('storeAssets', () => {
   const stubEvents = chainSyncData(ChainSyncDataSet.WithMint);
   let queryRunner: QueryRunner;
   let buffer: TypeormStabilityWindowBuffer;
+  let dataSource$: Observable<DataSource>;
   const entities = [BlockEntity, BlockDataEntity, AssetEntity];
 
   const project$ = () =>
@@ -30,7 +31,7 @@ describe('storeAssets', () => {
     }).pipe(
       Mappers.withMint(),
       withTypeormTransaction({
-        dataSource$: defer(() => from(initializeDataSource({ entities }))),
+        dataSource$,
         logger
       }),
       storeBlock(),
@@ -44,6 +45,7 @@ describe('storeAssets', () => {
 
   beforeEach(async () => {
     const dataSource = await initializeDataSource({ entities });
+    dataSource$ = of(dataSource);
     queryRunner = dataSource.createQueryRunner();
     buffer = new TypeormStabilityWindowBuffer({ allowNonSequentialBlockHeights: true, logger });
     await buffer.initialize(queryRunner);
@@ -57,13 +59,50 @@ describe('storeAssets', () => {
   it('inserts assets on mint, deletes when 1st mint block is rolled back', async () => {
     const repository = queryRunner.manager.getRepository(AssetEntity);
     const mintEvent = await projectTilFirst((evt) => evt.mint.length > 0);
+    expect(Object.values(mintEvent.mintedAssetTotalSupplies)).toHaveLength(mintEvent.mint.length);
+    const firstMintedAssetTotalSupply = mintEvent.mintedAssetTotalSupplies[mintEvent.mint[0].assetId]!;
+    expect(firstMintedAssetTotalSupply).toBeGreaterThan(0);
     expect(await repository.count()).toBe(mintEvent.mint.length);
     const firstDbMint = await repository.findOne({ where: { id: mintEvent.mint[0].assetId } });
     expect(firstDbMint?.supply).toBe(mintEvent.mint[0].quantity);
-    await projectTilFirst(
+    const rollbackEvent = await projectTilFirst(
       (evt) =>
         evt.block.header.hash === mintEvent.block.header.hash && evt.eventType === ChainSyncEventType.RollBackward
     );
     expect(await repository.count()).toBe(0);
+    expect(rollbackEvent.mintedAssetTotalSupplies[rollbackEvent.mint[0].assetId]).toEqual(
+      firstMintedAssetTotalSupply - mintEvent.mint[0].quantity
+    );
+  });
+
+  it('increments asset supply on 2nd mint, decrements when 2st mint block is rolled back', async () => {
+    // Find some asset that has been minted more than once
+    const numberOfAssetMintEvents: Record<Cardano.AssetId, number> = {};
+    const secondMintEvent = await projectTilFirst((evt) => {
+      for (const { assetId } of evt.mint) {
+        if (evt.eventType === ChainSyncEventType.RollForward) {
+          numberOfAssetMintEvents[assetId] = (numberOfAssetMintEvents[assetId] || 0) + 1;
+        } else {
+          numberOfAssetMintEvents[assetId] = (numberOfAssetMintEvents[assetId] || 0) - 1;
+        }
+        if (numberOfAssetMintEvents[assetId] > 1) {
+          return true;
+        }
+      }
+      return false;
+    });
+    expect(secondMintEvent.mint.length).toBeGreaterThan(0);
+    const assetIdThatWasMintedTwice = Object.entries(numberOfAssetMintEvents).find(
+      ([_, numberOfMints]) => numberOfMints > 1
+    )![0] as Cardano.AssetId;
+    const totalSupplyAfter2ndMint = secondMintEvent.mintedAssetTotalSupplies[assetIdThatWasMintedTwice]!;
+    expect(totalSupplyAfter2ndMint).not.toBeUndefined();
+    const rollbackEvent = await projectTilFirst(
+      (evt) =>
+        evt.eventType === ChainSyncEventType.RollBackward && evt.block.header.hash === secondMintEvent.block.header.hash
+    );
+    expect(secondMintEvent.mint.length).toBeGreaterThan(0);
+    expect(rollbackEvent.mintedAssetTotalSupplies[assetIdThatWasMintedTwice]).toBeLessThan(totalSupplyAfter2ndMint);
+    expect(rollbackEvent.mintedAssetTotalSupplies[assetIdThatWasMintedTwice]).toBeGreaterThan(0);
   });
 });
