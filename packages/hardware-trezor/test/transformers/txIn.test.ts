16ffc5da44fd7916e6738594c89b8222ce3aaa19
Dominik Guzei <dominik.guzei@gmail.com>
2023-06-05 15:05:57 +0200

    chore(hardware-trezor): cleanup unit tests based on feedback

--- packages/hardware-trezor/test/transformers/txIn.test.ts
+++ packages/hardware-trezor/test/transformers/txIn.test.ts
@@ -1,40 +1,37 @@
 import { contextWithKnownAddresses, contextWithoutKnownAddresses, knownAddressKeyPath, txIn } from '../testData';
 import { mapTxIns, toTrezorTxIn } from '../../src';
 
+const expectedTrezorTxInWithKnownAddress = {
+  path: knownAddressKeyPath,
+  prev_hash: txIn.txId,
+  prev_index: txIn.index
+};
+
+const expectedTrezorTxInWithoutKnownAddress = {
+  path: undefined,
+  prev_hash: txIn.txId,
+  prev_index: txIn.index
+};
+
 describe('tx-inputs', () => {
   describe('toTrezorTxIn', () => {
     it('maps a simple tx input from an unknown third party address', async () => {
-      const trezorTxIn = await toTrezorTxIn(txIn, contextWithoutKnownAddresses);
-      expect(trezorTxIn).toEqual({
-        path: undefined,
-        prev_hash: Buffer.from(txIn.txId).toString('hex'),
-        prev_index: txIn.index
-      });
+      const mappedTrezorTxIn = await toTrezorTxIn(txIn, contextWithoutKnownAddresses);
+      expect(mappedTrezorTxIn).toEqual(expectedTrezorTxInWithoutKnownAddress);
     });
     it('maps a simple tx input from a known address', async () => {
-      const trezorTxIn = await toTrezorTxIn(txIn, contextWithKnownAddresses);
-      expect(trezorTxIn).toEqual({
-        path: knownAddressKeyPath,
-        prev_hash: Buffer.from(txIn.txId).toString('hex'),
-        prev_index: txIn.index
-      });
+      const mappedTrezorTxIn = await toTrezorTxIn(txIn, contextWithKnownAddresses);
+      expect(mappedTrezorTxIn).toEqual(expectedTrezorTxInWithKnownAddress);
     });
   });
   describe('mapTxIns', () => {
     it('can map a a set of TxIns', async () => {
-      const txIns = await mapTxIns([txIn, txIn, txIn], CONTEXT_WITH_KNOWN_ADDRESSES);
-
-      expect(txIns.length).toEqual(3);
-
-      for (const input of txIns) {
-        expect(input).toEqual({
-          path: knownAddressKeyPath,
-          prev_hash: txIn.txId,
-          prev_index: txIn.index
-        });
-      }
-
-      expect.assertions(4);
+      const txIns = await mapTxIns([txIn, txIn, txIn], contextWithKnownAddresses);
+      expect(txIns).toEqual([
+        expectedTrezorTxInWithKnownAddress,
+        expectedTrezorTxInWithKnownAddress,
+        expectedTrezorTxInWithKnownAddress
+      ]);
     });
   });
 });
