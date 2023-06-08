d0505b2800f8c75675fbc93c4310400b551bde34
John Oshalusi <john.oshalusi@iohk.io>
2023-06-08 11:39:15 +0100

    feat: remove createdAt and updatedAt

--- packages/tx-construction/test/tx-builder/TxBuilder.test.ts
+++ packages/tx-construction/test/tx-builder/TxBuilder.test.ts
@@ -9,8 +9,8 @@ import {
   TransactionSigner,
   util
 } from '@cardano-sdk/key-management';
+import { Asset, CML, Cardano, Handle, ProviderError, ProviderFailure } from '@cardano-sdk/core';
 import { AssetId, mockProviders as mocks, somePartialStakePools } from '@cardano-sdk/util-dev';
-import { CML, Cardano, Handle, ProviderError, ProviderFailure, Uri } from '@cardano-sdk/core';
 import {
   GenericTxBuilder,
   HandleNotFoundError,
@@ -32,17 +32,13 @@ function assertObjectRefsAreDifferent(obj1: unknown, obj2: unknown): void {
 }
 
 const resolvedHandle = {
-  backgroundImage: 'ipfs://alice' as Uri,
-  createdAt: {
-    hash: Cardano.BlockId('7a48b034645f51743550bbaf81f8a14771e58856e031eb63844738ca8ad72298'),
-    slot: Cardano.Slot(100)
-  },
+  backgroundImage: Asset.Uri('ipfs://alice'),
   defaultInWallet: 'alice',
   handle: 'alice',
   hasDatum: false,
-  image: 'ipfs://image' as Uri,
+  image: Asset.Uri('ipfs://image'),
   policyId: Cardano.PolicyId('b0d07d45fe9514f80213f4020e5a61241458be626841cde717cb38a7'),
-  profilePic: 'ipfs://pix' as Uri,
+  profilePic: Asset.Uri('ipfs://pix'),
   resolvedAddresses: {
     cardano: Cardano.PaymentAddress('addr_test1vr8nl4u0u6fmtfnawx2rxfz95dy7m46t6dhzdftp2uha87syeufdg')
   },
