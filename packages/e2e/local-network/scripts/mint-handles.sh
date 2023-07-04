5fd65bc46f7786fe1a272dba01627584999d09f3
Daniele Ricci <daniele.ricci@iohk.io>
2023-07-04 11:43:45 +0200

    chore(e2e): refactor nft minting scripts to not use the faucet

--- packages/e2e/local-network/scripts/mint-handles.sh
+++ packages/e2e/local-network/scripts/mint-handles.sh
@@ -28,7 +28,7 @@ EOL
 echo $policyid > /sdk-ipc/handle_policy_ids
 
 addr=$(cardano-cli address build --payment-verification-key-file network-files/utxo-keys/utxo1.vkey --testnet-magic 888)
-faucetAddr="addr_test1qqen0wpmhg7fhkus45lyv4wju26cecgu6avplrnm6dgvuk6qel5hu3u3q0fht53ly97yx95hkt56j37ch07pesf6s4pqh5gd4e"
+destAddr="addr_test1qr0c3frkem9cqn5f73dnvqpena27k2fgqew6wct9eaka03agfwkvzr0zyq7nqvcj24zehrshx63zzdxv24x3a4tcnfeq9zwmn7"
 utxo=$(cardano-cli query utxo --address "$addr" --testnet-magic 888 | awk 'NR == 3 {printf("%s#%s", $1, $2)}')
 
 tokenList="1 ${policyid}.${handleHexes[0]}+1 ${policyid}.${handleHexes[1]}+2 ${policyid}.${handleHexes[2]}"
@@ -37,7 +37,7 @@ cardano-cli transaction build \
   --babbage-era \
   --change-address "$addr" \
   --tx-in "$utxo" \
-  --tx-out "$faucetAddr"+10000000+"$tokenList" \
+  --tx-out "$destAddr"+10000000+"$tokenList" \
   --mint "$tokenList" \
   --mint-script-file network-files/utxo-keys/minting-policy.json \
   --metadata-json-file network-files/utxo-keys/handles-metadata.json \
