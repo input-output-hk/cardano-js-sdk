91e1e7a772bbaa8a0d412ea53b9e86d21ab35d57
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-07-04 09:44:49 +0300

    refactor(core): loosen arg type of isPlutusData utils to accept 'unknown'

--- packages/core/src/Cardano/util/isPlutusData.ts
+++ packages/core/src/Cardano/util/isPlutusData.ts
@@ -1,20 +1,19 @@
-import { ConstrPlutusData, PlutusData, PlutusList, PlutusMap } from '../types';
+import { ConstrPlutusData, PlutusList, PlutusMap } from '../types';
 
-export const isPlutusBoundedBytes = (plutusData: PlutusData): plutusData is Uint8Array =>
-  ArrayBuffer.isView(plutusData);
+export const isPlutusBoundedBytes = (plutusData: unknown): plutusData is Uint8Array => ArrayBuffer.isView(plutusData);
 
 export const isAnyPlutusDataCollection = (
-  plutusData: PlutusData
+  plutusData: unknown
 ): plutusData is PlutusList | PlutusMap | ConstrPlutusData =>
   typeof plutusData === 'object' && !isPlutusBoundedBytes(plutusData);
 
-export const isPlutusList = (plutusData: PlutusData): plutusData is PlutusList =>
+export const isPlutusList = (plutusData: unknown): plutusData is PlutusList =>
   isAnyPlutusDataCollection(plutusData) && 'items' in plutusData;
 
-export const isPlutusMap = (plutusData: PlutusData): plutusData is PlutusMap =>
+export const isPlutusMap = (plutusData: unknown): plutusData is PlutusMap =>
   isAnyPlutusDataCollection(plutusData) && 'data' in plutusData;
 
-export const isConstrPlutusData = (plutusData: PlutusData): plutusData is ConstrPlutusData =>
+export const isConstrPlutusData = (plutusData: unknown): plutusData is ConstrPlutusData =>
   isAnyPlutusDataCollection(plutusData) && 'fields' in plutusData;
 
-export const isPlutusBigInt = (plutusData: PlutusData): plutusData is bigint => typeof plutusData === 'bigint';
+export const isPlutusBigInt = (plutusData: unknown): plutusData is bigint => typeof plutusData === 'bigint';
