e654196ed1b037c3d0d141c95ddf45522fe055e3
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-07-25 15:23:08 +0300

    refactor: update nullable TypeORM entity field types to include '| null'
    querying a nullable column can return 'null' - entity types should reflect that

--- packages/projection-typeorm/src/entity/Handle.entity.ts
+++ packages/projection-typeorm/src/entity/Handle.entity.ts
@@ -6,8 +6,8 @@ import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 export class HandleEntity {
   @PrimaryColumn()
   handle?: string;
-  @Column({ nullable: true })
-  cardanoAddress?: Cardano.PaymentAddress;
+  @Column({ nullable: true, type: 'varchar' })
+  cardanoAddress?: Cardano.PaymentAddress | null;
   @OneToOne(() => AssetEntity, { onDelete: 'CASCADE' })
   @JoinColumn()
   asset?: AssetEntity;
