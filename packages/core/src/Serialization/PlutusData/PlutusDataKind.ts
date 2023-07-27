41145f087f786bb4f41ee48a3aed7a753a39e7c6
Angel Castillo <Angel.Castillo@iohk.io>
2023-07-27 15:02:17 +0800

    feat(core): added custom PlutusData serialization classes

---
+++ packages/core/src/Serialization/PlutusData/PlutusDataKind.ts
@@ -0,0 +1,29 @@
+/**
+ * The plutus data type kind.
+ */
+export enum PlutusDataKind {
+  /**
+   * Represents a specific constructor of a 'Sum Type' along with its arguments.
+   */
+  ConstrPlutusData,
+
+  /**
+   * A map of PlutusData as both key and values.
+   */
+  Map,
+
+  /**
+   * A list of PlutusData.
+   */
+  List,
+
+  /**
+   * An integer.
+   */
+  Integer,
+
+  /**
+   * Bounded bytes.
+   */
+  Bytes
+}
