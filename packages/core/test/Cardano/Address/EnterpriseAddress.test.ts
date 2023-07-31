9b647b77ddaa9364bf0aea32b3aded8661a832c9
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-07-31 14:53:43 +0300

    fix(core): use correct address type when building EnterpriseScript from credential

--- packages/core/test/Cardano/Address/EnterpriseAddress.test.ts
+++ packages/core/test/Cardano/Address/EnterpriseAddress.test.ts
@@ -2,11 +2,21 @@ import * as cip19TestVectors from './Cip19TestVectors';
 import { Cardano } from '../../../src';
 
 describe('Cardano/Address/EnterpriseAddress', () => {
-  it('fromCredentials can build the correct EnterpriseAddress instance', () => {
-    const address = Cardano.EnterpriseAddress.fromCredentials(
-      Cardano.NetworkId.Mainnet,
-      cip19TestVectors.KEY_PAYMENT_CREDENTIAL
-    );
-    expect(address.toAddress().toBech32()).toEqual(cip19TestVectors.enterpriseKey);
+  describe('fromCredentials', () => {
+    it('can build the correct EnterpriseAddress instance with key hash type', () => {
+      const address = Cardano.EnterpriseAddress.fromCredentials(
+        Cardano.NetworkId.Mainnet,
+        cip19TestVectors.KEY_PAYMENT_CREDENTIAL
+      );
+      expect(address.toAddress().toBech32()).toEqual(cip19TestVectors.enterpriseKey);
+    });
+
+    it('can build the correct EnterpriseAddress instance with script hash type', () => {
+      const address = Cardano.EnterpriseAddress.fromCredentials(
+        Cardano.NetworkId.Mainnet,
+        cip19TestVectors.SCRIPT_CREDENTIAL
+      );
+      expect(address.toAddress().toBech32()).toEqual(cip19TestVectors.enterpriseScript);
+    });
   });
 });
