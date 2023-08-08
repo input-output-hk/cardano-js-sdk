060ac99315ae3339d4e208f5578d587cf35a7dc2
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-08-08 10:09:12 +0300

    feat(projection): add cip67.byAssetId
    that can be used to look up the label and decoded value by asset id

--- packages/projection/src/operators/Mappers/withCIP67.ts
+++ packages/projection/src/operators/Mappers/withCIP67.ts
@@ -15,30 +15,32 @@ export interface CIP67Asset {
 export interface WithCIP67 {
   cip67: {
     byLabel: Partial<Record<Asset.AssetNameLabel, CIP67Asset[]>>;
+    byAssetId: Partial<Record<Cardano.AssetId, CIP67Asset>>;
   };
 }
 
-export const withCIP67 = unifiedProjectorOperator<WithUtxo, WithCIP67>((evt) => ({
-  ...evt,
-  cip67: {
-    byLabel: groupBy(
-      evt.utxo.produced
-        .flatMap((utxo) =>
-          [...(utxo[1].value.assets?.keys() || [])].map((assetId): CIP67Asset | null => {
-            const assetName = Cardano.AssetId.getAssetName(assetId);
-            const decoded = Asset.AssetNameLabel.decode(assetName);
-            if (!decoded) return null;
-            return {
-              assetId,
-              assetName,
-              decoded,
-              policyId: Cardano.AssetId.getPolicyId(assetId),
-              utxo
-            };
-          })
-        )
-        .filter(isNotNil),
-      (e) => e.decoded.label
+export const withCIP67 = unifiedProjectorOperator<WithUtxo, WithCIP67>((evt) => {
+  const cip67Assets = evt.utxo.produced
+    .flatMap((utxo) =>
+      [...(utxo[1].value.assets?.keys() || [])].map((assetId): CIP67Asset | null => {
+        const assetName = Cardano.AssetId.getAssetName(assetId);
+        const decoded = Asset.AssetNameLabel.decode(assetName);
+        if (!decoded) return null;
+        return {
+          assetId,
+          assetName,
+          decoded,
+          policyId: Cardano.AssetId.getPolicyId(assetId),
+          utxo
+        };
+      })
     )
-  }
-}));
+    .filter(isNotNil);
+  return {
+    ...evt,
+    cip67: {
+      byAssetId: Object.fromEntries(cip67Assets.map((asset) => [asset.assetId, asset])),
+      byLabel: groupBy(cip67Assets, (e) => e.decoded.label)
+    }
+  };
+});
--- packages/projection/test/operators/Mappers/withCIP67.test.ts
+++ packages/projection/test/operators/Mappers/withCIP67.test.ts
@@ -4,53 +4,53 @@ import { firstValueFrom, of } from 'rxjs';
 import { generateRandomHexString } from '@cardano-sdk/util-dev';
 
 describe('withCIP67', () => {
-  describe('byLabel', () => {
-    it('collects all cip67-compliant assets and groups them by label', async () => {
-      const txId1 = generateRandomHexString(64);
-      const policyId533 = generateRandomHexString(56);
-      const assetName533 = '00215410123';
-      const assetId533 = `${policyId533}${assetName533}`;
-      const evt = {
-        utxo: {
-          consumed: [] as Cardano.TxIn[],
-          produced: [
-            [
-              { index: 0, txId: txId1 },
-              {
-                address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
-                value: {
-                  assets: new Map([
-                    [`${generateRandomHexString(56)}011d7690234`, 1n],
-                    [assetId533, 2n]
-                  ]),
-                  coins: 123n
-                }
-              }
-            ],
-            [
-              { index: 1, txId: txId1 },
-              {
-                address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
-                value: {
-                  assets: new Map([[`${generateRandomHexString(56)}00017650345`, 1n]]),
-                  coins: 123n
-                }
-              }
-            ],
-            [
-              { index: 2, txId: generateRandomHexString(64) },
-              {
-                address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
-                value: {
-                  assets: new Map([[`${generateRandomHexString(56)}00017650345`, 3n]]),
-                  coins: 123n
-                }
-              }
-            ]
-          ]
-        }
-      } as ProjectionEvent<Mappers.WithUtxo>;
+  const txId1 = generateRandomHexString(64);
+  const policyId533 = generateRandomHexString(56);
+  const assetName533 = '00215410123';
+  const assetId533 = Cardano.AssetId(`${policyId533}${assetName533}`);
+  const evt = {
+    utxo: {
+      consumed: [] as Cardano.TxIn[],
+      produced: [
+        [
+          { index: 0, txId: txId1 },
+          {
+            address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
+            value: {
+              assets: new Map([
+                [`${generateRandomHexString(56)}011d7690234`, 1n],
+                [assetId533, 2n]
+              ]),
+              coins: 123n
+            }
+          }
+        ],
+        [
+          { index: 1, txId: txId1 },
+          {
+            address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
+            value: {
+              assets: new Map([[`${generateRandomHexString(56)}00017650345`, 1n]]),
+              coins: 123n
+            }
+          }
+        ],
+        [
+          { index: 2, txId: generateRandomHexString(64) },
+          {
+            address: Cardano.PaymentAddress('addr_test1wrsexavz37208qda7mwwu4k7hcpg26cz0ce86f5e9kul3hqzlh22t'),
+            value: {
+              assets: new Map([[`${generateRandomHexString(56)}00017650345`, 3n]]),
+              coins: 123n
+            }
+          }
+        ]
+      ]
+    }
+  } as ProjectionEvent<Mappers.WithUtxo>;
 
+  describe('collects all cip67-compliant assets', () => {
+    it('groups them by label', async () => {
       const { cip67 } = await firstValueFrom(of(evt).pipe(Mappers.withCIP67()));
       expect(Object.keys(cip67.byLabel)).toHaveLength(3);
       expect(cip67.byLabel[Asset.AssetNameLabel(533)]).toEqual([
@@ -66,6 +66,21 @@ describe('withCIP67', () => {
         } as Mappers.CIP67Asset
       ]);
       expect(cip67.byLabel[Asset.AssetNameLabel(23)]).toHaveLength(2);
+    });
+
+    it('groups them by asset id', async () => {
+      const { cip67 } = await firstValueFrom(of(evt).pipe(Mappers.withCIP67()));
+      expect(Object.keys(cip67.byAssetId)).toHaveLength(4);
+      expect(cip67.byAssetId[assetId533]).toEqual({
+        assetId: assetId533,
+        assetName: assetName533,
+        decoded: {
+          content: Cardano.AssetName('123'),
+          label: 533
+        },
+        policyId: policyId533,
+        utxo: evt.utxo.produced[0]
+      } as Mappers.CIP67Asset);
     });
   });
 });
