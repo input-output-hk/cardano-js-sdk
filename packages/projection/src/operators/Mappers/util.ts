8beb2e22b476649c72caa532b46d4799a4772ee5
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-08-31 10:30:49 +0300

    refactor(projection): dedupe withHandles and withHandleMetadata implementations
    - hoist assetNameToUTF8Handle to util.ts
    - use AssetName.toUTF8 core util
    
    also, replace custom error with the existing InvalidStringError,
    because it wasn't used anywhere

--- packages/projection/src/operators/Mappers/util.ts
+++ packages/projection/src/operators/Mappers/util.ts
@@ -1,5 +1,14 @@
+import { Asset, Cardano, Handle } from '@cardano-sdk/core';
+import { InvalidStringError } from '@cardano-sdk/util';
+
 /**
  * Up to 100k transactions per block.
  * Fits in 64-bit signed integer.
  */
 export const computeCompactTxId = (blockHeight: number, txIndex: number) => blockHeight * 100_000 + txIndex;
+
+export const assetNameToUTF8Handle = (assetName: Cardano.AssetName): Handle => {
+  const handle = Cardano.AssetName.toUTF8(assetName);
+  if (!Asset.util.isValidHandle(handle)) throw new InvalidStringError(`Invalid handle ${handle}`);
+  return handle;
+};
