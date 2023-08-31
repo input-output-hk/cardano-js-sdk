2e73e8690bf98d2b6a0581ef81252b66e689f5a3
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2023-08-31 10:09:44 +0300

    feat(projection): add withHandleMetadata mapper
    it maps 'og' from base nft metadata and images from 'extra'

--- packages/projection/src/operators/Mappers/index.ts
+++ packages/projection/src/operators/Mappers/index.ts
@@ -2,6 +2,7 @@ export * from './certificates';
 export * from './withUtxo';
 export * from './withMint';
 export * from './withHandles';
+export * from './withHandleMetadata';
 export * from './withNftMetadata';
 export * from './withCIP67';
 export * from './withAddresses';
