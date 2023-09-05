a00a77df54a5cb707098af7d0947e0aa92eebeba
Daniele Ricci <daniele.ricci@iohk.io>
2023-09-05 08:57:39 +0200

    refactor(projection-typeorm): add explicit block slot field to pool registrations and retirements

--- packages/projection-typeorm/src/entity/PoolRegistration.entity.ts
+++ packages/projection-typeorm/src/entity/PoolRegistration.entity.ts
@@ -12,34 +12,50 @@ export class PoolRegistrationEntity {
   /** Computed from certificate pointer. Can be used to sort pool updates. */
   @PrimaryColumn(BigIntColumnOptions)
   id?: bigint;
+
   @Column()
   rewardAccount?: Cardano.RewardAccount;
+
   @Column(UInt64ColumnOptions)
   pledge?: bigint;
+
   @Column(UInt64ColumnOptions)
   cost?: bigint;
+
   // Review: should we store this as 'double' instead?
   // Maybe both formats? If we'll need to do computations with this
   // then it's best to keep the lossless format
   @Column({ type: 'jsonb' })
   margin?: Cardano.Fraction;
+
   @Column({ type: 'float4' })
   marginPercent?: Percent;
+
   @Column('jsonb')
   relays?: Cardano.Relay[];
+
   @Column('jsonb')
   owners?: Cardano.RewardAccount[];
+
   @Column({ length: 64, type: 'char' })
   vrf?: Cardano.VrfVkHex;
+
   @Column('varchar', { nullable: true })
   metadataUrl?: string | null;
+
   @Column({ length: 64, nullable: true, type: 'char' })
   metadataHash?: string | null;
+
   @JoinColumn()
   @ManyToOne(() => StakePoolEntity, (stakePool) => stakePool.registrations, OnDeleteCascadeRelationOptions)
   stakePool?: StakePoolEntity;
+
   @OneToOne(() => PoolMetadataEntity, (metadata) => metadata.poolUpdate)
   metadata?: PoolMetadataEntity | null;
+
+  @Column()
+  blockSlot?: Cardano.Slot;
+
   @ManyToOne(() => BlockEntity, OnDeleteCascadeRelationOptions)
   @JoinColumn()
   block?: BlockEntity;
