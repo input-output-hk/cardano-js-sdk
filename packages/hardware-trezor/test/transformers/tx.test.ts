2bbd84c6bffb63191f17aeba3e1d62a2d1fd1bb5
Tomislav Horaček <tomislav@tt-media.hr>
2023-10-30 13:30:03 +0100

    fix(hardware-trezor): fix tx mapping test

--- packages/hardware-trezor/test/transformers/tx.test.ts
+++ packages/hardware-trezor/test/transformers/tx.test.ts
@@ -78,17 +78,17 @@ describe('tx', () => {
         mint: [
           {
             policyId: '2a286ad895d091f2b3d168a6091ad2627d30a72761a5bc36eef00740',
-            tokenAmounts: [{ amount: '20', assetNameBytes: '' }]
+            tokenAmounts: [{ assetNameBytes: '', mintAmount: '20' }]
           },
           {
             policyId: '659f2917fb63f12b33667463ee575eeac1845bbc736b9c0bbc40ba82',
-            tokenAmounts: [{ amount: '-50', assetNameBytes: '54534c41' }]
+            tokenAmounts: [{ assetNameBytes: '54534c41', mintAmount: '-50' }]
           },
           {
             policyId: '7eae28af2208be856f7a119668ae52a49b73725e326dc16579dcc373',
             tokenAmounts: [
-              { amount: '40', assetNameBytes: '' },
-              { amount: '30', assetNameBytes: '504154415445' }
+              { assetNameBytes: '', mintAmount: '40' },
+              { assetNameBytes: '504154415445', mintAmount: '30' }
             ]
           }
         ],
