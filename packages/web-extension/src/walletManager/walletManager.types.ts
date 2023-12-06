ded7c3fd2f544439ff4347a1f105bf2e6c944097
Angel Castillo <angel.castillo@iohk.io>
2023-12-06 13:17:18 +0800

    feat(web-extension): add support for multiple wallets to the wallet manager class

--- packages/web-extension/src/walletManager/walletManager.types.ts
+++ packages/web-extension/src/walletManager/walletManager.types.ts
@@ -1,18 +1,61 @@
+import * as Crypto from '@cardano-sdk/crypto';
 import { AsyncKeyAgent } from '@cardano-sdk/key-management';
+import { Cardano } from '@cardano-sdk/core';
+import { Observable } from 'rxjs';
 import { ObservableWallet, storage } from '@cardano-sdk/wallet';
 
 export interface WalletManagerProps {
   walletName: string;
 }
 
+/** The wallet type */
+export enum WalletType {
+  MultiSignature = 'MultiSignature',
+  SingleSignature = 'SingleSignature'
+}
+
+/** The multi signature wallet type */
+export enum MultiSignatureWalletType {
+  RequireAllSignatures = 'RequireAllSignatures',
+  RequireAnySignaturesOf = 'RequireAnySignaturesOf',
+  RequireNSignaturesOf = 'RequireNSignaturesOf'
+}
+
+/** The single signature wallet properties */
+export type SingleSignatureWalletProps = {
+  __type: WalletType.SingleSignature;
+
+  /** The public key of the wallet */
+  accountIndex: number;
+};
+
+/** The multi signature wallet properties */
+export type MultiSignatureWalletProps = {
+  __type: WalletType.MultiSignature;
+
+  /** The public keys of the participants */
+  participants: Array<Crypto.Ed25519PublicKeyHex>;
+
+  /** The multi signature wallet type */
+  type: MultiSignatureWalletType;
+
+  /** Only required when the wallet type is `RequireNSignaturesOf`. */
+  n?: number;
+};
+
+/** The wallet properties */
+export type WalletProps = SingleSignatureWalletProps | MultiSignatureWalletProps;
+
 export interface WalletManagerActivateProps<P extends string | number = string, O = unknown> {
   /** User given name for the observableWallet being activated */
   observableWalletName: string;
-  /**
-   * Internal unique id calculated by {@link WalletManagerUi} based on the keyManager `chainId` and
-   * the root public key hash.
-   */
+
+  /** The wallet type */
+  walletProps: WalletProps;
+
+  /** Internal unique id calculated of the wallet. */
   walletId: string;
+
   /**
    * `provider` could be used to pass the necessary information to construct providers for different networks.
    * Its value is passed to the {@link WalletFactory}, which is required to create {@link WalletManagerWorker}.
@@ -51,11 +94,23 @@ export interface WalletManagerApi {
    * Reuses the store if the wallet was previously deactivated but not destroyed.
    */
   activate(props: WalletManagerActivateProps): Promise<void>;
+
+  /**
+   * Switches the network of the active wallet.
+   *
+   * @param id The chain id of the network to switch to.
+   */
+  switchNetwork(id: Cardano.ChainId): Promise<void>;
+
+  /** Returns an observable that emits the chain id when it changes. */
+  chainIdChanges(): Observable<Cardano.ChainId>;
+
   /**
    * Deactivate wallet. Wallet observable properties will emit only after a new wallet is {@link activate}ed.
    * The wallet store will be reused if the wallet is reactivated.
    */
   deactivate(): Promise<void>;
+
   /**
    * Deactivates the active wallet and destroy its existing store,
    * so that a future activation of the same wallet creates a new store.
