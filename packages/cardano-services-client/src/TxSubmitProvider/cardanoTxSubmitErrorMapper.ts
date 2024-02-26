f248e847b24d56cf5697b980ffaf494a151bb248
Mircea Hasegan <mircea.hasegan@iohk.io>
2024-02-26 13:59:25 +0100

    fix(cardano-services-client): parse json errors from cardano-submit-api

--- packages/cardano-services-client/src/TxSubmitProvider/cardanoTxSubmitErrorMapper.ts
+++ packages/cardano-services-client/src/TxSubmitProvider/cardanoTxSubmitErrorMapper.ts
@@ -1,20 +1,29 @@
 /* eslint-disable wrap-regex */
 import { TxSubmissionError, TxSubmissionErrorCode } from '@cardano-sdk/core';
 
+const parseStringishError = (errorData: string) => {
+  if (/outsideofvalidity/i.test(errorData)) {
+    return new TxSubmissionError(TxSubmissionErrorCode.OutsideOfValidityInterval, null, errorData);
+  }
+  if (/valuenotconserved/i.test(errorData)) {
+    return new TxSubmissionError(TxSubmissionErrorCode.ValueNotConserved, null, errorData);
+  }
+  if (/nonadacollateral/i.test(errorData)) {
+    return new TxSubmissionError(TxSubmissionErrorCode.NonAdaCollateral, null, errorData);
+  }
+  if (/incompletewithdrawals/i.test(errorData)) {
+    return new TxSubmissionError(TxSubmissionErrorCode.IncompleteWithdrawals, null, errorData);
+  }
+  return null;
+};
+
 export const mapCardanoTxSubmitError = (errorData: unknown): TxSubmissionError | null => {
   if (typeof errorData === 'string') {
-    if (/outsideofvalidity/i.test(errorData)) {
-      return new TxSubmissionError(TxSubmissionErrorCode.OutsideOfValidityInterval, null, errorData);
-    }
-    if (/valuenotconserved/i.test(errorData)) {
-      return new TxSubmissionError(TxSubmissionErrorCode.ValueNotConserved, null, errorData);
-    }
-    if (/nonadacollateral/i.test(errorData)) {
-      return new TxSubmissionError(TxSubmissionErrorCode.NonAdaCollateral, null, errorData);
-    }
-    if (/incompletewithdrawals/i.test(errorData)) {
-      return new TxSubmissionError(TxSubmissionErrorCode.IncompleteWithdrawals, null, errorData);
-    }
+    return parseStringishError(errorData);
+  } else if (typeof errorData === 'object' && errorData) {
+    // cardano-submit-api started returning json instead of raw string.
+    // For the moment, simply stringify it. In the future we may want to make use of it.
+    return parseStringishError(JSON.stringify(errorData));
   }
   return null;
 };
