32af676bf072668c69fb3e76e1d18632bc9fb68a
Gytis Ivaskevicius <me@gytis.io>
2024-04-26 12:58:59 +0300

    chore: fix autosetting kubeconfig context

--- nix/local/envs.nix
+++ nix/local/envs.nix
@@ -73,6 +73,8 @@ in {
     devshell.startup.setup.text = ''
       [ -e $PRJ_ROOT/.envrc.local ] && source $PRJ_ROOT/.envrc.local
       kubectl config use-context $K8S_USER
+      kubectl config use-context $K8S_USER --kubeconfig $PRJ_ROOT/.kube/us-east-2
+      kubectl config use-context $K8S_USER --kubeconfig $PRJ_ROOT/.kube/eu-central-1
       chmod 600 $PRJ_ROOT/.kube/*
     '';
   };
