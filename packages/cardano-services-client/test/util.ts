8c406caae91a6326b6f8c443d12219dc2222c500
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-05-10 13:41:48 +0100

    chore: cleanup Koralabs instances and replace with handleHttpProvider

--- packages/cardano-services-client/test/util.ts
+++ packages/cardano-services-client/test/util.ts
@@ -1,6 +1,5 @@
 import { Asset, Cardano, ProviderError, ProviderFailure } from '@cardano-sdk/core';
 import { AxiosError, AxiosResponse } from 'axios';
-import { IHandle, Rarity } from '@koralabs/handles-public-api-interfaces';
 import { logger } from '@cardano-sdk/util-dev';
 import { toSerializableObject } from '@cardano-sdk/util';
 
@@ -60,48 +59,4 @@ export const getBobHandleProviderResponse = {
   image: Asset.Uri('ipfs://c8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56fe'),
   policyId: Cardano.PolicyId('50fdcdbfa3154db86a87e4b5697ae30d272e0bbcfa8122efd3e301cb'),
   profilePic: Asset.Uri('ipfs://zrljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3yd1')
-};
-
-export const getAliceHandleAPIResponse: Partial<IHandle> = {
-  characters: 'rljm7n/23455',
-  created_slot_number: 33,
-  default_in_wallet: 'alice_default_hndle',
-  has_datum: false,
-  hex: '0f3abbc8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56fe0e78f19d9d5',
-  holder: 'stake1uyehkck0lajq8gr28t9uxnuvgcqrc6070x3k9r8048z8y5gh6ffgw',
-  image: 'ipfs://c8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56feasd',
-  length: 123,
-  name: 'alice',
-  numeric_modifiers: '-12.9',
-  og_number: 5,
-  rarity: Rarity.rare,
-  resolved_addresses: {
-    ada: 'addr_test1qqk4sr4f7vtqzd2w90d5nfu3n59jhhpawyphnek2y7er02nkrezryq3ydtmkg0e7e2jvzg443h0ffzfwd09wpcxy2fuqmcnecd'
-  },
-  standard_image: 'ipfs://c8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56feasdfasd',
-  updated_slot_number: 22,
-  utxo: 'rljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3ydtmkg0'
-};
-
-export const getBobHandleAPIResponse: Partial<IHandle> = {
-  bg_image: 'ipfs://zrljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3yd',
-  characters: 'rljm7n/23455',
-  created_slot_number: 33,
-  default_in_wallet: 'bob_default_handle',
-  has_datum: false,
-  hex: '0f3abbc8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56fe0e78f19d9d5',
-  holder: 'stake1uyehkck0lajq8gr28t9uxnuvgcqrc6070x3k9r8048z8y5gh6ffgw',
-  image: 'ipfs://c8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56fe',
-  length: 123,
-  name: 'bob',
-  numeric_modifiers: '-12.9',
-  og_number: 5,
-  pfp_image: 'ipfs://zrljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3yd1',
-  rarity: Rarity.rare,
-  resolved_addresses: {
-    ada: 'addr_test1qzrljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3ydtmkg0e7e2jvzg443h0ffzfwd09wpcxy2fuql9tk0g'
-  },
-  standard_image: 'ipfs://c8fc19c2e61bab6059bf8a466e6e754833a08a62a6c56feasdfasd',
-  updated_slot_number: 22,
-  utxo: 'rljm7nskakjydxlr450ktsj08zuw6aktvgfkmmyw9semrkrezryq3ydtmkg0'
 };
