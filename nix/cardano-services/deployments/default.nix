746ee5bb1f23d27107f3ea11ee28b7f46d67a06a
Daniele Ricci <daniele.ricci@iohk.io>
2024-05-10 16:23:08 +0200

    chore: remove --trace-gc from provider-server

--- nix/cardano-services/deployments/default.nix
+++ nix/cardano-services/deployments/default.nix
@@ -186,7 +186,6 @@ in
         providers = {
           backend = {
             enabled = true;
-            env.NODE_EXTRA_OPTIONS = "--trace-gc";
           };
           stake-pool-provider = {
             enabled = true;
@@ -331,7 +330,6 @@ in
           backend = {
             enabled = true;
             replicas = 3;
-            env.NODE_EXTRA_OPTIONS = "--trace-gc";
           };
           stake-pool-provider = {
             enabled = true;
@@ -404,7 +402,6 @@ in
         providers = {
           backend = {
             enabled = true;
-            env.NODE_EXTRA_OPTIONS = "--trace-gc";
           };
           stake-pool-provider = {
             enabled = true;
