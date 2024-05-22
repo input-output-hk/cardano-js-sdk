29a001482eec080a5bbea5a932c63c4adc35706b
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2024-05-22 09:05:27 +0300

    fix: remove null characters from user-specified strings when storing nft metadata
    also add a new chain sync data set in util-dev

--- packages/util-dev/src/chainSync/index.ts
+++ packages/util-dev/src/chainSync/index.ts
@@ -97,6 +97,7 @@ const intersect = (events: ChainSyncData['body'], points: PointOrOrigin[]) => {
 
 export enum ChainSyncDataSet {
   PreviewStakePoolProblem = 'preview-stake-pool-problem.json',
+  AssetNameUtf8Problem = 'asset-name-utf8-problem.json',
   WithPoolRetirement = 'with-pool-retirement.json',
   WithStakeKeyDeregistration = 'with-stake-key-deregistration.json',
   WithMint = 'with-mint.json',
