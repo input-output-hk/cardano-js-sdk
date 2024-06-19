c1a83293a9a7a1310634d1dc3588094e3d925cb3
Daniele Ricci <daniele.ricci@iohk.io>
2024-06-19 16:51:45 +0200

    chore: bump dependencies version

--- packages/e2e/local-network/scripts/install.sh
+++ packages/e2e/local-network/scripts/install.sh
@@ -24,10 +24,10 @@ mkdir -p bin
 echo "Download binaries from IOG build"
 case $(uname) in
 Darwin)
-  wget -O bin.tar.gz https://github.com/IntersectMBO/cardano-node/releases/download/${VERSION}-pre/cardano-node-${VERSION}-macos.tar.gz
+  wget -O bin.tar.gz https://github.com/IntersectMBO/cardano-node/releases/download/${VERSION}-sancho/cardano-node-${VERSION}-macos.tar.gz
   ;;
 Linux)
-  wget -O bin.tar.gz https://github.com/IntersectMBO/cardano-node/releases/download/${VERSION}-pre/cardano-node-${VERSION}-linux.tar.gz
+  wget -O bin.tar.gz https://github.com/IntersectMBO/cardano-node/releases/download/${VERSION}-sancho/cardano-node-${VERSION}-linux.tar.gz
   ;;
 esac
 
