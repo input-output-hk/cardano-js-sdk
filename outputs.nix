af207081347d3043f350217ca580bbe1479208fe
Gytis Ivaskevicius <me@gytis.io>
2024-06-28 12:35:42 +0300

    chore: move devshell from std to flake-parts

--- outputs.nix
+++ outputs.nix
@@ -8,6 +8,7 @@ inputs.flake-parts.lib.mkFlake {inherit inputs;} {
   imports = with inputs; [
     std.flakeModule
     devshell.flakeModule
+    ./nix/local/envs.nix
   ];
   systems = ["x86_64-linux" "aarch64-linux"];
 
@@ -15,7 +16,6 @@ inputs.flake-parts.lib.mkFlake {inherit inputs;} {
     cellsFrom = ./nix;
     cellBlocks = with std.blockTypes; [
       # Software Delivery Lifecycle (Local Development Environment)
-      (devshells "envs")
       (runnables "jobs")
       # Software Delivery Lifecycle (Packaging Layers)
       # For deeper context, please consult:
@@ -45,80 +45,5 @@ inputs.flake-parts.lib.mkFlake {inherit inputs;} {
     };
 
     legacyPackages.cardano-services = import ./nix/cardano-services/deployments {inherit pkgs nix-helm inputs;};
-
-    devshells.default = {
-      name = "cardano-js-sdk";
-
-      env = [
-        {
-          name = "KUBECONFIG";
-          eval = "$PRJ_ROOT/.kube/us-east-1";
-        }
-        {
-          name = "TF_VAR_PRJ_ROOT";
-          eval = "$PRJ_ROOT";
-        }
-      ];
-
-      packages = with pkgs; [
-        # Add packages that should be available in the devshell
-      ];
-
-      devshell.startup.source.text = ''
-        source $PRJ_ROOT/.envrc.local
-        kubectl config use-context $K8S_USER
-        kubectl config use-context $K8S_USER --kubeconfig $PRJ_ROOT/.kube/us-east-2
-        kubectl config use-context $K8S_USER --kubeconfig $PRJ_ROOT/.kube/eu-central-1
-        chmod 600 $PRJ_ROOT/.kube/*
-      '';
-
-      commands = let
-        mkK9sCommand = region: {
-          command = ''
-            if [ -z "$K8S_USER" ]; then
-              echo "To use this command you must set K8S_USER in $PRJ_ROOT/.access.local. See Readme."
-            else
-              ${pkgs.k9s}/bin/k9s --kubeconfig $PRJ_ROOT/.kube/${region} $@
-            fi
-          '';
-          name = "k9s-${region}";
-          category = "direct access";
-        };
-      in [
-        {package = pkgs.jq;}
-        {package = pkgs.treefmt;}
-        {package = pkgs.just;}
-        {package = pkgs.yq-go;}
-        {
-          package = pkgs.sops;
-          name = "sops";
-          category = "cloud";
-        }
-        {
-          package = pkgs.awscli2;
-          name = "aws";
-          category = "cloud";
-        }
-        {
-          package = pkgs.eksctl;
-          category = "cloud";
-        }
-        {
-          package = pkgs.k9s;
-          category = "kubernetes";
-        }
-        {
-          package = pkgs.kubectl;
-          category = "kubernetes";
-        }
-        {
-          package = pkgs.kubernetes-helm;
-          category = "kubernetes";
-        }
-        (mkK9sCommand "us-east-1")
-        (mkK9sCommand "us-east-2")
-        (mkK9sCommand "eu-central-1")
-      ];
-    };
   };
 }
