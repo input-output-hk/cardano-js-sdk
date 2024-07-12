d655f62b51adab600ebfbcd033f1537e25eaa68a
Mircea Hasegan <mircea.hasegan@iohk.io>
2024-07-12 16:28:17 +0200

    chore: update configuration to 6d7fac44d6a877ae9a3cf5eeda163ae539b17774
    subrepo:
      subdir:   "packages/cardano-services/config"
      merged:   "6d7fac44d6"
    upstream:
      origin:   "https://github.com/input-output-hk/cardano-configurations.git"
      branch:   "master"
      commit:   "6d7fac44d6"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- packages/cardano-services/config/.github/scripts/download-all.sh
+++ packages/cardano-services/config/.github/scripts/download-all.sh
@@ -13,7 +13,7 @@ mkdir -p \
 
 wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/topology.json -O network/$CARDANO_NETWORK/cardano-node/topology.json
 wget -qO- $CARDANO_CONFIG_URL/$CARDANO_NETWORK/config.json \
-  | jq '.ByronGenesisFile = "../genesis/byron.json" | .ShelleyGenesisFile = "../genesis/shelley.json" | .AlonzoGenesisFile = "../genesis/alonzo.json" | .ConwayGenesisFile = "../genesis/conway.json"' \
+  | jq '.ByronGenesisFile = "../genesis/byron.json" | .ShelleyGenesisFile = "../genesis/shelley.json" | .AlonzoGenesisFile = "../genesis/alonzo.json"' \
   | jq '.' > network/$CARDANO_NETWORK/cardano-node/config.json
 wget -qO- $CARDANO_CONFIG_URL/$CARDANO_NETWORK/db-sync-config.json \
   | jq '.NodeConfigFile = "../cardano-node/config.json"' \
@@ -25,4 +25,12 @@ wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/submit-api-config.json -O network/$
 wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/byron-genesis.json -O network/$CARDANO_NETWORK/genesis/byron.json
 wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/shelley-genesis.json -O network/$CARDANO_NETWORK/genesis/shelley.json
 wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/alonzo-genesis.json -O network/$CARDANO_NETWORK/genesis/alonzo.json
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/conway-genesis.json -O network/$CARDANO_NETWORK/genesis/conway.json
+if wget --spider $CARDANO_CONFIG_URL/$CARDANO_NETWORK/conway-genesis.json 2>/dev/null; then
+  wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/conway-genesis.json -O network/$CARDANO_NETWORK/genesis/conway.json
+  mv network/$CARDANO_NETWORK/cardano-node/config.json network/$CARDANO_NETWORK/cardano-node/config.json.tmp
+  cat network/$CARDANO_NETWORK/cardano-node/config.json.tmp \
+  | jq '.ConwayGenesisFile = "../genesis/conway.json"' \
+  | jq '.' > network/$CARDANO_NETWORK/cardano-node/config.json
+  rm network/$CARDANO_NETWORK/cardano-node/config.json.tmp
+fi
+
