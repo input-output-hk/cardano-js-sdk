061e12a5afbe527832996a04458a40f06baceebb
Ola <32410436+ce0la@users.noreply.github.com>
2024-07-15 15:44:42 +0100

    Remove direnv dependency and wire up devshell (#1350)
    * Remove direnv dependency and wire up devshell
    
    * chore: move devshell from std to flake-parts
    
    * Add output.nix to flake.nix
    Update flake.lock
    Clean up .env template
    
    * chore: remove pkgs overwrite
    
    * chore: run treefmt on nix files
    
    ---------
    
    Co-authored-by: Gytis Ivaskevicius <me@gytis.io>

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Cardano JS SDK";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -19,31 +21,36 @@
     };
   };
 
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # still used by single-user-mode (e.g. ci)
+    extra-substituters = [
+      "https://cache.iog.io"
+      "s3://lace-nix-cache?region=us-east-1"
+    ];
+    allow-import-from-derivation = "true";
+  };
+  # --------------------------------------------------------------
+
   outputs = {
     std,
+    self,
     nix-helm,
+    devshell,
     ...
   } @ inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = with inputs; [
+      imports = [
         std.flakeModule
         devshell.flakeModule
+        ./nix/local/envs.nix
       ];
-      systems = ["x86_64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: {
-        legacyPackages.cardano-services = import ./nix/cardano-services/deployments {inherit pkgs nix-helm inputs;};
-      };
-
-      std.grow = {
+      std.grow = with inputs; {
         cellsFrom = ./nix;
         cellBlocks = with std.blockTypes; [
           # Software Delivery Lifecycle (Local Development Environment)
-          (devshells "envs")
           (runnables "jobs")
           # Software Delivery Lifecycle (Packaging Layers)
           # For deeper context, please consult:
@@ -52,6 +59,20 @@
           (runnables "operables")
           (containers "oci-images" {ci.publish = true;})
         ];
+      };
+
+      std.harvest = {
+        packages = [["local" "packages"] ["local" "jobs"]];
+        devShells = [["local" "envs"] ["desktop" "envs"]];
+        hydraJobs = ["desktop" "hydraJobs"];
+      };
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        legacyPackages.cardano-services = import ./nix/cardano-services/deployments {inherit pkgs nix-helm inputs;};
       };
     };
 }
