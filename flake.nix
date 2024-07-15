fce389aaa9a9c00fd26aca93c7e425218fc734d2
ce0la <oshunboteo@gmail.com>
2024-07-15 12:58:27 +0100

    Add output.nix to flake.nix Update flake.lock Clean up .env template

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
 
     n2c.url = "github:nlewo/nix2container";
     n2c.inputs.nixpkgs.follows = "nixpkgs";
@@ -15,7 +14,7 @@
     nix-helm.inputs.nixpkgs.follows = "nixpkgs";
 
     std = {
-      url = "github:divnix/std/v0.24.0-1";
+      url = "github:divnix/std";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.n2c.follows = "n2c";
       inputs.devshell.follows = "devshell";
@@ -29,18 +28,55 @@
       "https://cache.iog.io"
       "s3://lace-nix-cache?region=us-east-1"
     ];
-    extra-trusted-substituters = [
-      "https://cache.iog.io"
-      "s3://lace-nix-cache?region=us-east-1"
-    ];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "nixbuild.net/lace@iohk.io-1:sEHlRBG/EcTkef5vJx2LmDPxpe8kln81TQuZyb9TdJY="
-      "lace:doMHHeFIW0/T/Gw5y+S6OfhRVC5Imhm28rptgdLRBn4="
-    ];
     allow-import-from-derivation = "true";
   };
   # --------------------------------------------------------------
 
-  outputs = inputs: import ./outputs.nix inputs;
+  outputs = {
+    std,
+    self,
+    nix-helm,
+    devshell,
+    ...
+  } @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        std.flakeModule
+        devshell.flakeModule
+        ./nix/local/envs.nix
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+
+      std.grow = with inputs; {
+        cellsFrom = ./nix;
+        cellBlocks = with std.blockTypes; [
+          # Software Delivery Lifecycle (Local Development Environment)
+          (runnables "jobs")
+          # Software Delivery Lifecycle (Packaging Layers)
+          # For deeper context, please consult:
+          #   https://std.divnix.com/patterns/four-packaging-layers.html
+          (installables "packages" {ci.build = true;})
+          (runnables "operables")
+          (containers "oci-images" {ci.publish = true;})
+        ];
+      };
+
+      std.harvest = {
+        packages = [["local" "packages"] ["local" "jobs"]];
+        devShells = [["local" "envs"] ["desktop" "envs"]];
+        hydraJobs = ["desktop" "hydraJobs"];
+      };
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        legacyPackages.cardano-services = import ./nix/cardano-services/deployments {inherit pkgs nix-helm inputs;};
+      };
+    };
 }
