f06fa1c6a8f2ccf038cb5f338010f57547ad2c45
Daniele Ricci <daniele.ricci@iohk.io>
2024-07-29 15:32:04 +0200

    chore: disable asset-provider-server in all dev envs

--- nix/cardano-services/deployments/default.nix
+++ nix/cardano-services/deployments/default.nix
@@ -207,7 +207,7 @@ in
               env.OVERRIDE_FUZZY_OPTIONS = "true";
             };
             handle-provider.enabled = true;
-            asset-provider.enabled = true;
+            #asset-provider.enabled = true;
             chain-history-provider.enabled = true;
           };
 
@@ -356,7 +356,7 @@ in
               env.OVERRIDE_FUZZY_OPTIONS = "true";
             };
             handle-provider.enabled = true;
-            asset-provider.enabled = true;
+            #asset-provider.enabled = true;
             chain-history-provider = {
               enabled = true;
               replicas = 2;
@@ -409,7 +409,7 @@ in
               env.OVERRIDE_FUZZY_OPTIONS = "true";
             };
             handle-provider.enabled = true;
-            asset-provider.enabled = true;
+            #asset-provider.enabled = true;
             chain-history-provider.enabled = true;
           };
 
