e687b719497112a91c891615868ccaec872370c1
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2024-08-22 21:14:00 +0100

    fix: export TxMetadata module from core package root

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * as Asset from './Asset';
 export * as Cardano from './Cardano';
 export * as Serialization from './Serialization';
+export * as TxMetadata from './TxMetadata';
 export * from './Provider';
 export * from './util';
 export * from './errors';
