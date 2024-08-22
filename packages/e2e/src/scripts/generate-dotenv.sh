39343e71ab713a2f21825ddec6445c812806077e
Daniele Ricci <daniele.ricci@iohk.io>
2024-08-22 13:23:14 +0200

    test: add option to choose target cluster for deployed environments e2e tests

--- packages/e2e/src/scripts/generate-dotenv.sh
+++ packages/e2e/src/scripts/generate-dotenv.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+set -e
 set -x
 set -o
 
-environment="$1"
+case $2 in
+  any)
+    environment="$1"
+    ;;
+  *)
+    environment="$1.$2"
+    ;;
+esac
 
-url="https://${environment}.lw.iog.io"
+domain="${environment}.lw.iog.io"
+url="https://${domain}/"
 
 # Construct the environment file content
 envFileContent="
@@ -16,19 +26,22 @@ KEY_MANAGEMENT_PROVIDER=inMemory
 
 # Providers setup - required by getWallet
 TEST_CLIENT_ASSET_PROVIDER=http
-TEST_CLIENT_ASSET_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_ASSET_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
+TEST_CLIENT_CHAIN_HISTORY_PROVIDER=http
+TEST_CLIENT_CHAIN_HISTORY_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_HANDLE_PROVIDER=http
-TEST_CLIENT_HANDLE_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4011/\"}'
+TEST_CLIENT_HANDLE_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_NETWORK_INFO_PROVIDER=http
-TEST_CLIENT_NETWORK_INFO_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_NETWORK_INFO_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_REWARDS_PROVIDER=http
-TEST_CLIENT_REWARDS_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_REWARDS_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_TX_SUBMIT_PROVIDER=http
-TEST_CLIENT_TX_SUBMIT_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_TX_SUBMIT_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_UTXO_PROVIDER=http
-TEST_CLIENT_UTXO_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_UTXO_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
 TEST_CLIENT_STAKE_POOL_PROVIDER=http
-TEST_CLIENT_STAKE_POOL_PROVIDER_PARAMS='{\"baseUrl\":\"$url:4000/\"}'
+TEST_CLIENT_STAKE_POOL_PROVIDER_PARAMS='{\"baseUrl\":\"${url}\"}'
+WS_PROVIDER_URL='wss://${domain}/ws'
 "
 
 # Write the environment file content to the specified file
