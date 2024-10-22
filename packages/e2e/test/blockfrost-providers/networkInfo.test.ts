b4ce30b429b3b715cf9ea5ea88437413843fbd5f
Daniele Ricci <daniele.ricci@iohk.io>
2024-10-22 11:08:36 +0200

    test(e2e): add comparison test skeleton for blockfrost network info provider

---
+++ packages/e2e/test/blockfrost-providers/networkInfo.test.ts
@@ -0,0 +1,26 @@
+// cSpell:ignore cardano utxos
+
+import { NetworkInfoProvider } from '@cardano-sdk/core';
+import { logger } from '@cardano-sdk/util-dev';
+import { networkInfoHttpProvider } from '@cardano-sdk/cardano-services-client';
+import { toSerializableObject } from '@cardano-sdk/util';
+
+// LW-11697 to enable this
+describe.skip('Web Socket', () => {
+  const legacyProvider = networkInfoHttpProvider({ baseUrl: 'http://localhost:4000/', logger });
+  const provider = networkInfoHttpProvider({ baseUrl: 'http://localhost:4001/', logger });
+
+  const methods: (keyof NetworkInfoProvider)[] = [
+    'eraSummaries',
+    'genesisParameters',
+    'lovelaceSupply',
+    'protocolParameters',
+    'stake'
+  ];
+
+  test.each(methods)('compare %s', async (method) => {
+    const [legacyResponse, response] = await Promise.all([legacyProvider[method](), provider[method]()]);
+
+    expect(toSerializableObject(response)).toEqual(toSerializableObject(legacyResponse));
+  });
+});
