a76691b70347235a37da9cd52fe9175cbd62d38a
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2024-11-19 17:58:06 +0000

    feat(cardano-services-client): extend Blockfrost request pipeline to include body
    Needed for transaction submission
    
    Co-authored-by: Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
    Co-authored-by: Martynas Kazlauskas <martynas.kazlauskas@iohk.io>

--- packages/cardano-services-client/src/blockfrost/BlockfrostProvider.ts
+++ packages/cardano-services-client/src/blockfrost/BlockfrostProvider.ts
@@ -36,12 +36,13 @@ export abstract class BlockfrostProvider implements Provider {
 
   /**
    * @param endpoint e.g. 'blocks/latest'
+   * @param requestInit request options
    * @throws {ProviderError}
    */
-  protected async request<T>(endpoint: string): Promise<T> {
+  protected async request<T>(endpoint: string, requestInit?: RequestInit): Promise<T> {
     try {
       this.logger.debug('request', endpoint);
-      const response = await this.#client.request<T>(endpoint);
+      const response = await this.#client.request<T>(endpoint, requestInit);
       this.logger.debug('response', response);
       return response;
     } catch (error) {
