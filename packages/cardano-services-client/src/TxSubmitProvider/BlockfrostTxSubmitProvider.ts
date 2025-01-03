68fb8c8ee63e31a23fd6191cd0b4ba8a21708a2d
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2025-01-03 18:18:31 +0200

    fix(cardano-services-client): map Blockfrost ValueNotConserved and OutsideOfValidityInterval errors

--- packages/cardano-services-client/src/TxSubmitProvider/BlockfrostTxSubmitProvider.ts
+++ packages/cardano-services-client/src/TxSubmitProvider/BlockfrostTxSubmitProvider.ts
@@ -1,6 +1,76 @@
 import { BlockfrostClient, BlockfrostProvider } from '../blockfrost';
 import { Logger } from 'ts-log';
-import { SubmitTxArgs, TxSubmitProvider } from '@cardano-sdk/core';
+import {
+  ProviderError,
+  SubmitTxArgs,
+  TxSubmissionError,
+  TxSubmissionErrorCode,
+  TxSubmitProvider,
+  ValueNotConservedData
+} from '@cardano-sdk/core';
+
+type BlockfrostTxSubmissionErrorMessage = {
+  contents: {
+    contents: {
+      contents: {
+        error: [string];
+      };
+    };
+  };
+};
+
+const tryParseBlockfrostTxSubmissionErrorMessage = (
+  errorMessage: string
+): BlockfrostTxSubmissionErrorMessage | null => {
+  try {
+    const error = JSON.parse(errorMessage);
+    if (typeof error === 'object' && Array.isArray(error?.contents?.contents?.contents?.error)) {
+      return error;
+    }
+  } catch {
+    return null;
+  }
+  return null;
+};
+
+/**
+ * @returns TxSubmissionError if sucessfully mapped, otherwise `null`
+ */
+const tryMapTxBlockfrostSubmissionError = (error: ProviderError): TxSubmissionError | null => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const detail = JSON.parse(error.detail as any);
+    if (typeof detail?.message === 'string') {
+      const blockfrostTxSubmissionErrorMessage = tryParseBlockfrostTxSubmissionErrorMessage(detail.message);
+      if (!blockfrostTxSubmissionErrorMessage) {
+        return null;
+      }
+      const message = blockfrostTxSubmissionErrorMessage.contents.contents.contents.error[0];
+      if (message.includes('OutsideValidityIntervalUTxO')) {
+        // error also contains information about validity interval and actual slots,
+        // but we're currently not using this info
+        return new TxSubmissionError(TxSubmissionErrorCode.OutsideOfValidityInterval, null, message);
+      }
+      // eslint-disable-next-line wrap-regex
+      const valueNotConservedMatch = /ValueNotConservedUTxO.+Coin (\d+).+Coin (\d+)/.exec(message);
+      if (valueNotConservedMatch) {
+        const consumed = BigInt(valueNotConservedMatch[1]);
+        const produced = BigInt(valueNotConservedMatch[2]);
+        const valueNotConservedData: ValueNotConservedData = {
+          // error also contains information about consumed and produced native assets
+          // but we're currently not using this info
+          consumed: { coins: consumed },
+          produced: { coins: produced }
+        };
+        return new TxSubmissionError(TxSubmissionErrorCode.ValueNotConserved, valueNotConservedData, message);
+      }
+    }
+  } catch {
+    return null;
+  }
+
+  return null;
+};
 
 export class BlockfrostTxSubmitProvider extends BlockfrostProvider implements TxSubmitProvider {
   constructor(client: BlockfrostClient, logger: Logger) {
@@ -9,10 +79,20 @@ export class BlockfrostTxSubmitProvider extends BlockfrostProvider implements Tx
 
   async submitTx({ signedTransaction }: SubmitTxArgs): Promise<void> {
     // @ todo handle context and resolutions
-    await this.request<string>('tx/submit', {
-      body: Buffer.from(signedTransaction, 'hex'),
-      headers: { 'Content-Type': 'application/cbor' },
-      method: 'POST'
-    });
+    try {
+      await this.request<string>('tx/submit', {
+        body: Buffer.from(signedTransaction, 'hex'),
+        headers: { 'Content-Type': 'application/cbor' },
+        method: 'POST'
+      });
+    } catch (error) {
+      if (error instanceof ProviderError) {
+        const submissionError = tryMapTxBlockfrostSubmissionError(error);
+        if (submissionError) {
+          throw new ProviderError(error.reason, submissionError, error.detail);
+        }
+      }
+      throw error;
+    }
   }
 }
