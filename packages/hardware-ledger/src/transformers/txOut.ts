b658c825190ef18849941d49af0a55e4e2e3749c
John Oshalusi <john.oshalusi@iohk.io>
2025-02-26 11:34:27 +0100

    feat: sign multi-sig transaction with trezor

--- packages/hardware-ledger/src/transformers/txOut.ts
+++ packages/hardware-ledger/src/transformers/txOut.ts
@@ -5,16 +5,6 @@ import { LedgerTxTransformerContext } from '../types';
 import { mapTokenMap } from './assets';
 import { util } from '@cardano-sdk/key-management';
 
-const isScriptAddress = (address: string): boolean => {
-  const baseAddress = Cardano.Address.fromBech32(address).asBase();
-  const paymentCredential = baseAddress?.getPaymentCredential();
-  const stakeCredential = baseAddress?.getStakeCredential();
-  return (
-    paymentCredential?.type === Cardano.CredentialType.ScriptHash &&
-    stakeCredential?.type === Cardano.CredentialType.ScriptHash
-  );
-};
-
 const toInlineDatum: Transform<Cardano.PlutusData, Ledger.Datum> = (datum) => ({
   datumHex: Serialization.PlutusData.fromCore(datum).toCbor(),
   type: Ledger.DatumType.INLINE
@@ -30,9 +20,9 @@ const toDestination: Transform<Cardano.TxOut, Ledger.TxOutputDestination, Ledger
   context
 ) => {
   const knownAddress = context?.knownAddresses.find((address) => address.address === txOut.address);
-  const isScriptWallet = isScriptAddress(txOut.address);
+  const isScriptAddress = Cardano.util.isScriptAddress(txOut.address);
 
-  if (knownAddress && !isScriptWallet) {
+  if (knownAddress && !isScriptAddress) {
     const paymentKeyPath = util.paymentKeyPathFromGroupedAddress(knownAddress);
     const stakeKeyPath = util.stakeKeyPathFromGroupedAddress(knownAddress);
 
