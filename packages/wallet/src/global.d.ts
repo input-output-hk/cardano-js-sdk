3a21307dd2b713ea49be4a048027326612cc8ea7
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2025-11-05 17:21:17 +0100

    style(wallet): fix prettier formatting in global.d.ts
    Fix formatting issues:
    - Add missing semicolon after export statement
    - Reformat method signatures to single line per prettier rules

--- packages/wallet/src/global.d.ts
+++ packages/wallet/src/global.d.ts
@@ -1,15 +1,9 @@
-export {}
+export {};
 
 declare global {
   interface Array<T> {
-    findLastIndex(
-      predicate: (value: T, index: number, obj: T[]) => unknown,
-      thisArg?: any
-    ): number;
+    findLastIndex(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): number;
 
-    findLast(
-      predicate: (value: T, index: number, obj: T[]) => unknown,
-      thisArg?: any
-    ): T | undefined;
+    findLast(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): T | undefined;
   }
 }
